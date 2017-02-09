0971088df7dfae8f266ab5f683a78afc8c10677e
Kraig Amador <Kraig.Amador@ticketmaster.com>
2017-02-09 12:23:31 -0800

    Commented out the kubeclient code since we are likely able to do this very easily with nodeports

--- main.go
+++ main.go
@@ -4,20 +4,22 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"reflect"
 	"strings"
 
-	albcontroller "git.tm.tmcs/kubernetes/alb-ingress/pkg/cmd/controller"
+	"github.com/kylelemons/godebug/pretty"
+
 	"git.tm.tmcs/kubernetes/alb-ingress/pkg/config"
 	"k8s.io/ingress/core/pkg/ingress"
 	"k8s.io/ingress/core/pkg/ingress/controller"
 	"k8s.io/ingress/core/pkg/ingress/defaults"
 	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 )
 
 type ALBController struct {
 	ingress.Controller
-	kubeClient *internalclientset.Clientset
+	// kubeClient        *internalclientset.Clientset
+	lastUpdatePayload *ingress.Configuration
 }
 
 func main() {
@@ -25,7 +27,7 @@ func main() {
 	ic := controller.NewIngressController(ac)
 
 	// We need a kubeClient to look up the nginx daemonset IPs
-	ac.kubeClient = albcontroller.KubeClient()
+	// ac.kubeClient = albcontroller.KubeClient()
 
 	defer func() {
 		log.Printf("Shutting down ingress controller...")
@@ -56,8 +58,30 @@ func (ac ALBController) Test(file string) *exec.Cmd {
 	return exec.Command("echo", file)
 }
 
+// data, err := ic.cfg.Backend.OnUpdate(ingress.Configuration{
+// 	Backends:            upstreams,
+// 	Servers:             servers,
+// 	TCPEndpoints:        ic.getStreamServices(ic.cfg.TCPConfigMapName, api.ProtocolTCP),
+// 	UPDEndpoints:        ic.getStreamServices(ic.cfg.UDPConfigMapName, api.ProtocolUDP),
+// 	PassthroughBackends: passUpstreams,
+// })
+// sync executes the above and then a Reload
 func (ac ALBController) OnUpdate(updatePayload ingress.Configuration) ([]byte, error) {
 	log.Printf("Received OnUpdate notification")
+
+	// Abort processing if updatePayload hasnt changed
+	log.Printf(pretty.Compare(ac.lastUpdatePayload, updatePayload))
+	log.Printf("set the lastUpdatePayload")
+	ac.lastUpdatePayload = &updatePayload
+	log.Printf("new diff")
+	log.Printf(pretty.Compare(ac.lastUpdatePayload, updatePayload))
+
+	if reflect.DeepEqual(ac.lastUpdatePayload, updatePayload) {
+		log.Printf("Nothing new!!")
+		return []byte(``), nil
+	}
+
+	// Prints backends
 	for _, b := range updatePayload.Backends {
 		eps := []string{}
 		for _, e := range b.Endpoints {
@@ -65,9 +89,14 @@ func (ac ALBController) OnUpdate(updatePayload ingress.Configuration) ([]byte, e
 		}
 		log.Printf("%v: %v", b.Name, strings.Join(eps, ", "))
 	}
+
+	// Prints servers
 	for _, b := range updatePayload.Servers {
 		log.Printf("%v", b.Hostname)
 	}
+
+	// Really dont think we will ever use these bytes since we are not writing
+	// configuration files. there are a lot of assumptions in the core ingress
 	return []byte(`<string containing a configuration file>`), nil
 }
 
