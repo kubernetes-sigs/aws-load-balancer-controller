f54d0ba56a3aa4a82ecc099211468df4b7fd94c8
Kraig Amador <Kraig.Amador@ticketmaster.com>
2017-02-09 12:23:31 -0800

    Commented out the kubeclient code since we are likely able to do this very easily with nodeports

--- pkg/cmd/controller/util.go
+++ pkg/cmd/controller/util.go
@@ -1,139 +1,126 @@
 package controller
 
-import (
-	"flag"
-	"os"
-	"time"
+// const (
+// 	// High enough QPS to fit all expected use cases. QPS=0 is not set here, because
+// 	// client code is overriding it.
+// 	defaultQPS = 1e6
+// 	// High enough Burst to fit all expected use cases. Burst=0 is not set here, because
+// 	// client code is overriding it.
+// 	defaultBurst = 1e6
+// )
 
-	"github.com/golang/glog"
-	"github.com/spf13/pflag"
-	"k8s.io/kubernetes/pkg/api"
-	client "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-	"k8s.io/kubernetes/pkg/client/unversioned/clientcmd"
-	clientcmdapi "k8s.io/kubernetes/pkg/client/unversioned/clientcmd/api"
-)
+// // KubeClient re-parses the parameters since the core package does not expose the client information we need
+// func KubeClient() *client.Clientset {
+// 	var (
+// 		flags = pflag.NewFlagSet("", pflag.ExitOnError)
 
-const (
-	// High enough QPS to fit all expected use cases. QPS=0 is not set here, because
-	// client code is overriding it.
-	defaultQPS = 1e6
-	// High enough Burst to fit all expected use cases. Burst=0 is not set here, because
-	// client code is overriding it.
-	defaultBurst = 1e6
-)
+// 		apiserverHost = flags.String("apiserver-host", "", "The address of the Kubernetes Apiserver "+
+// 			"to connect to in the format of protocol://address:port, e.g., "+
+// 			"http://localhost:8080. If not specified, the assumption is that the binary runs inside a "+
+// 			"Kubernetes cluster and local discovery is attempted.")
+// 		kubeConfigFile = flags.String("kubeconfig", "", "Path to kubeconfig file with authorization and master location information.")
 
-// KubeClient re-parses the parameters since the core package does not expose the client information we need
-func KubeClient() *client.Clientset {
-	var (
-		flags = pflag.NewFlagSet("", pflag.ExitOnError)
+// 		_ = flags.String("default-backend-service", "",
+// 			`Service used to serve a 404 page for the default backend. Takes the form
+//     	namespace/name. The controller uses the first node port of this Service for
+//     	the default backend.`)
 
-		apiserverHost = flags.String("apiserver-host", "", "The address of the Kubernetes Apiserver "+
-			"to connect to in the format of protocol://address:port, e.g., "+
-			"http://localhost:8080. If not specified, the assumption is that the binary runs inside a "+
-			"Kubernetes cluster and local discovery is attempted.")
-		kubeConfigFile = flags.String("kubeconfig", "", "Path to kubeconfig file with authorization and master location information.")
+// 		_ = flags.String("ingress-class", "",
+// 			`Name of the ingress class to route through this controller.`)
 
-		_ = flags.String("default-backend-service", "",
-			`Service used to serve a 404 page for the default backend. Takes the form
-    	namespace/name. The controller uses the first node port of this Service for
-    	the default backend.`)
+// 		_ = flags.String("configmap", "",
+// 			`Name of the ConfigMap that contains the custom configuration to use`)
 
-		_ = flags.String("ingress-class", "",
-			`Name of the ingress class to route through this controller.`)
+// 		_ = flags.String("publish-service", "",
+// 			`Service fronting the ingress controllers. Takes the form
+//  		namespace/name. The controller will set the endpoint records on the
+//  		ingress objects to reflect those on the service.`)
 
-		_ = flags.String("configmap", "",
-			`Name of the ConfigMap that contains the custom configuration to use`)
+// 		_ = flags.String("tcp-services-configmap", "",
+// 			`Name of the ConfigMap that contains the definition of the TCP services to expose.
+// 		The key in the map indicates the external port to be used. The value is the name of the
+// 		service with the format namespace/serviceName and the port of the service could be a
+// 		number of the name of the port.
+// 		The ports 80 and 443 are not allowed as external ports. This ports are reserved for the backend`)
 
-		_ = flags.String("publish-service", "",
-			`Service fronting the ingress controllers. Takes the form
- 		namespace/name. The controller will set the endpoint records on the
- 		ingress objects to reflect those on the service.`)
+// 		_ = flags.String("udp-services-configmap", "",
+// 			`Name of the ConfigMap that contains the definition of the UDP services to expose.
+// 		The key in the map indicates the external port to be used. The value is the name of the
+// 		service with the format namespace/serviceName and the port of the service could be a
+// 		number of the name of the port.`)
 
-		_ = flags.String("tcp-services-configmap", "",
-			`Name of the ConfigMap that contains the definition of the TCP services to expose.
-		The key in the map indicates the external port to be used. The value is the name of the
-		service with the format namespace/serviceName and the port of the service could be a 
-		number of the name of the port.
-		The ports 80 and 443 are not allowed as external ports. This ports are reserved for the backend`)
+// 		_ = flags.Duration("sync-period", 60*time.Second,
+// 			`Relist and confirm cloud resources this often.`)
 
-		_ = flags.String("udp-services-configmap", "",
-			`Name of the ConfigMap that contains the definition of the UDP services to expose.
-		The key in the map indicates the external port to be used. The value is the name of the
-		service with the format namespace/serviceName and the port of the service could be a 
-		number of the name of the port.`)
+// 		_ = flags.String("watch-namespace", api.NamespaceAll,
+// 			`Namespace to watch for Ingress. Default is to watch all namespaces`)
 
-		_ = flags.Duration("sync-period", 60*time.Second,
-			`Relist and confirm cloud resources this often.`)
+// 		_ = flags.Int("healthz-port", 10254, "port for healthz endpoint.")
 
-		_ = flags.String("watch-namespace", api.NamespaceAll,
-			`Namespace to watch for Ingress. Default is to watch all namespaces`)
+// 		_ = flags.Bool("profiling", true, `Enable profiling via web interface host:port/debug/pprof/`)
 
-		_ = flags.Int("healthz-port", 10254, "port for healthz endpoint.")
+// 		_ = flags.String("default-ssl-certificate", "", `Name of the secret
+// 		that contains a SSL certificate to be used as default for a HTTPS catch-all server`)
 
-		_ = flags.Bool("profiling", true, `Enable profiling via web interface host:port/debug/pprof/`)
+// 		_ = flags.String("health-check-path", "/healthz", `Defines
+// 		the URL to be used as health check inside in the default server in NGINX.`)
 
-		_ = flags.String("default-ssl-certificate", "", `Name of the secret 
-		that contains a SSL certificate to be used as default for a HTTPS catch-all server`)
+// 		_ = flags.Bool("update-status", true, `Indicates if the
+// 		ingress controller should update the Ingress status IP/hostname. Default is true`)
+// 	)
 
-		_ = flags.String("health-check-path", "/healthz", `Defines 
-		the URL to be used as health check inside in the default server in NGINX.`)
+// 	flags.Usage = func() {}
 
-		_ = flags.Bool("update-status", true, `Indicates if the 
-		ingress controller should update the Ingress status IP/hostname. Default is true`)
-	)
+// 	flags.AddGoFlagSet(flag.CommandLine)
+// 	flags.Parse(os.Args)
 
-	flags.Usage = func() {}
+// 	kubeClient, err := createApiserverClient(*apiserverHost, *kubeConfigFile)
+// 	if err != nil {
+// 		handleFatalInitError(err)
+// 	}
 
-	flags.AddGoFlagSet(flag.CommandLine)
-	flags.Parse(os.Args)
+// 	return kubeClient
+// }
 
-	kubeClient, err := createApiserverClient(*apiserverHost, *kubeConfigFile)
-	if err != nil {
-		handleFatalInitError(err)
-	}
+// // createApiserverClient creates new Kubernetes Apiserver client. When kubeconfig or apiserverHost param is empty
+// // the function assumes that it is running inside a Kubernetes cluster and attempts to
+// // discover the Apiserver. Otherwise, it connects to the Apiserver specified.
+// //
+// // apiserverHost param is in the format of protocol://address:port/pathPrefix, e.g.http://localhost:8001.
+// // kubeConfig location of kubeconfig file
+// func createApiserverClient(apiserverHost string, kubeConfig string) (*client.Clientset, error) {
 
-	return kubeClient
-}
+// 	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+// 		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfig},
+// 		&clientcmd.ConfigOverrides{ClusterInfo: clientcmdapi.Cluster{Server: apiserverHost}})
 
-// createApiserverClient creates new Kubernetes Apiserver client. When kubeconfig or apiserverHost param is empty
-// the function assumes that it is running inside a Kubernetes cluster and attempts to
-// discover the Apiserver. Otherwise, it connects to the Apiserver specified.
-//
-// apiserverHost param is in the format of protocol://address:port/pathPrefix, e.g.http://localhost:8001.
-// kubeConfig location of kubeconfig file
-func createApiserverClient(apiserverHost string, kubeConfig string) (*client.Clientset, error) {
+// 	cfg, err := clientConfig.ClientConfig()
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
-		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfig},
-		&clientcmd.ConfigOverrides{ClusterInfo: clientcmdapi.Cluster{Server: apiserverHost}})
+// 	cfg.QPS = defaultQPS
+// 	cfg.Burst = defaultBurst
+// 	cfg.ContentType = "application/vnd.kubernetes.protobuf"
 
-	cfg, err := clientConfig.ClientConfig()
-	if err != nil {
-		return nil, err
-	}
+// 	glog.Infof("Creating API server client for %s", cfg.Host)
 
-	cfg.QPS = defaultQPS
-	cfg.Burst = defaultBurst
-	cfg.ContentType = "application/vnd.kubernetes.protobuf"
+// 	client, err := client.NewForConfig(cfg)
 
-	glog.Infof("Creating API server client for %s", cfg.Host)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return client, nil
+// }
 
-	client, err := client.NewForConfig(cfg)
-
-	if err != nil {
-		return nil, err
-	}
-	return client, nil
-}
-
-/**
- * Handles fatal init error that prevents server from doing any work. Prints verbose error
- * message and quits the server.
- */
-func handleFatalInitError(err error) {
-	glog.Fatalf("Error while initializing connection to Kubernetes apiserver. "+
-		"This most likely means that the cluster is misconfigured (e.g., it has "+
-		"invalid apiserver certificates or service accounts configuration). Reason: %s\n"+
-		"Refer to the troubleshooting guide for more information: "+
-		"https://github.com/kubernetes/ingress/blob/master/docs/troubleshooting.md", err)
-}
+// /**
+//  * Handles fatal init error that prevents server from doing any work. Prints verbose error
+//  * message and quits the server.
+//  */
+// func handleFatalInitError(err error) {
+// 	glog.Fatalf("Error while initializing connection to Kubernetes apiserver. "+
+// 		"This most likely means that the cluster is misconfigured (e.g., it has "+
+// 		"invalid apiserver certificates or service accounts configuration). Reason: %s\n"+
+// 		"Refer to the troubleshooting guide for more information: "+
+// 		"https://github.com/kubernetes/ingress/blob/master/docs/troubleshooting.md", err)
+// }
