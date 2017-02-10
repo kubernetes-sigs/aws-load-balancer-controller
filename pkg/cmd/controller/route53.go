bff88ae6e991564a487dd6d4f45e86aed903d86b
joshrosso <joshrosso@gmail.com>
2017-02-10 15:08:25 -0700

    Remove deprecated session.New usage

--- pkg/cmd/controller/route53.go
+++ pkg/cmd/controller/route53.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"log"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
@@ -11,8 +12,15 @@ type Route53 struct {
 }
 
 func newRoute53(awsconfig *aws.Config) *Route53 {
+	session, err := session.NewSession(awsconfig)
+	if err != nil {
+		log.Printf("Failed to create AWS session. Error: %s.", err.Error())
+		return nil
+	}
+
 	r := Route53{
-		route53.New(session.New(awsconfig)),
+		//route53.New(session.New(awsconfig)),
+		route53.New(session),
 	}
 	return &r
 }
