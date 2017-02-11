f9a61a1d8a7610e9c7500b6ddb85f56901a617ad
joshrosso <joshrosso@gmail.com>
2017-02-11 15:41:12 -0700

    Add initial ALB creation

--- pkg/cmd/controller/elbv2.go
+++ pkg/cmd/controller/elbv2.go
@@ -5,6 +5,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/elbv2"
 	"github.com/golang/glog"
+	"fmt"
 )
 
 // ELBV2 is our extension to AWS's elbv2.ELBV2
@@ -19,8 +20,36 @@ func newELBV2(awsconfig *aws.Config) *ELBV2 {
 		return nil
 	}
 
+	// Temporary for tests
+	// TODO: Auto-resovle
+	region :="us-west-1"
+	session.Config.Region = &region
+
 	elbv2 := ELBV2{
 		elbv2.New(session),
 	}
 	return &elbv2
+}
+
+// initial function to test creation of ALB
+// WIP
+func (elb *ELBV2) createALB(a *albIngress) error {
+	// this should automatically be resolved up stack
+	// TODO: Remove once resolving correctly
+	a.clusterName = "TEMPCLUSTERNAME"
+	albName := fmt.Sprintf("%s-%s", a.clusterName, a.serviceName)
+	alb := &elbv2.CreateLoadBalancerInput{
+		Name: &albName,
+		Subnets: []*string{aws.String("subnet-dc76ecb8"), aws.String("subnet-1510bb4d")},
+	}
+
+	resp, err := elb.CreateLoadBalancer(alb)
+
+	if err != nil {
+		fmt.Printf("ALB CREATION FAILED: %s", err.Error())
+		return err
+	}
+
+	fmt.Printf("ALB CREATION SUCCEEDED: %s", resp)
+	return nil
 }
