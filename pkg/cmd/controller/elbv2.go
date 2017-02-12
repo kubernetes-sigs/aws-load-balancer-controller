7d0348d7bd84e5dbf3c8b0086369446d11498849
joshrosso <joshrosso@gmail.com>
2017-02-11 21:12:58 -0700

    Add initial target map; refactor create method

--- pkg/cmd/controller/elbv2.go
+++ pkg/cmd/controller/elbv2.go
@@ -47,17 +47,22 @@ func newELBV2(awsconfig *aws.Config) *ELBV2 {
 
 // initial function to test creation of ALB
 // WIP
+// TODO: This massive method will be refactored ;)
 func (elb *ELBV2) createALB(a *albIngress) error {
 	// Verify subnet keys are present before starting ALB creation.
 	if a.annotations[subnet1Key] == "" || a.annotations[subnet2Key] == "" {
 		return errors.New("One or both ALB subnet annotations missing. Canceling ALB creation.")
 	}
-
 	// this should automatically be resolved up stack
 	// TODO: Remove once resolving correctly
 	a.clusterName = "TEMPCLUSTERNAME"
-
 	albName := fmt.Sprintf("%s-%s", a.clusterName, a.serviceName)
+
+	tGroupResp, err := elb.createTargetGroup(a, &albName)
+	if err != nil { return err }
+	err = elb.registerTargets(a, tGroupResp)
+	if err != nil { return err }
+
 	alb := &elbv2.CreateLoadBalancerInput{
 		Name: &albName,
 		Subnets: []*string{aws.String(a.annotations[subnet1Key]), aws.String(a.annotations[subnet2Key])},
@@ -68,21 +73,26 @@ func (elb *ELBV2) createALB(a *albIngress) error {
 		fmt.Printf("ALB CREATION FAILED: %s", err.Error())
 		return err
 	}
-
 	fmt.Printf("ALB CREATION SUCCEEDED: %s", resp)
+	elb.LoadBalancer = resp.LoadBalancers[0]
+	return nil
+}
+
+func (elb *ELBV2) createTargetGroup(a *albIngress, albName *string) (*elbv2.CreateTargetGroupOutput, error) {
 	descRequest := &ec2.DescribeSubnetsInput { SubnetIds: []*string{aws.String(a.annotations[subnet1Key])}, }
 	subnetInfo, err := elb.EC2.DescribeSubnets(descRequest)
 
+
 	if err != nil {
 		fmt.Printf("Failed to lookup vpcID before creating target group: %s", err.Error())
-		return err
+		return nil, err
 	}
 
 	vpcID := subnetInfo.Subnets[0].VpcId
 
-	// target group in VPC for which ALB will route to
+	// Target group in VPC for which ALB will route to
 	targetParams := &elbv2.CreateTargetGroupInput{
-		Name: aws.String(albName),
+		Name: albName,
 		Port: aws.Int64(int64(a.nodePort)),
 		Protocol: aws.String("HTTP"),
 		VpcId: vpcID,
@@ -92,12 +102,48 @@ func (elb *ELBV2) createALB(a *albIngress) error {
 
 	if err != nil {
 		fmt.Printf("Target Group failed to create: %s", err.Error())
+		return nil, err
+	}
+	fmt.Printf("Target Group CREATION SUCCEEDED: %s", tGroupResp)
+	return tGroupResp, err
+}
+
+func (elb *ELBV2) registerTargets(a *albIngress, tGroupResp *elbv2.CreateTargetGroupOutput) error {
+
+	descRequest := &ec2.DescribeSubnetsInput { SubnetIds: []*string{aws.String(a.annotations[subnet1Key])}, }
+	subnetInfo, err := elb.EC2.DescribeSubnets(descRequest)
+	// ugly hack to get all instanceIds for a VPC;
+	// we'll eventually go through k8s api
+	// TODO: Remove all this in favor of introspecting instanceIds from
+	//       albIngress struct
+	ec2InstanceFilter := &ec2.Filter{
+		Name: aws.String("vpc-id"),
+		Values: []*string{subnetInfo.Subnets[0].VpcId},
+	}
+	descInstParams := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{ec2InstanceFilter},
+	}
+	instances, err := elb.EC2.DescribeInstances(descInstParams)
+
+	// Instance registration for target group
+	targets := []*elbv2.TargetDescription{}
+	fmt.Printf("are there node ids?!?!?: %s", instances.Reservations[0].Instances)
+	for _, reservation := range instances.Reservations {
+		for _, instance := range reservation.Instances {
+			targets = append(targets, &elbv2.TargetDescription{Id: instance.InstanceId, Port: aws.Int64(int64(a.nodePort))})
+		}
+	}
+	registerParams := &elbv2.RegisterTargetsInput{
+		TargetGroupArn: tGroupResp.TargetGroups[0].TargetGroupArn,
+		Targets: targets,
+	}
+	fmt.Printf("Targets to register were: %s", registerParams)
+	rTargetResp, err := elb.RegisterTargets(registerParams)
+	if err != nil {
+		fmt.Printf("Failed to register targets to group: %s", err.Error())
 		return err
 	}
-
-	fmt.Printf("Target Group CREATION SUCCEEDED: %s", tGroupResp)
-
-
-	elb.LoadBalancer = resp.LoadBalancers[0]
+	fmt.Printf("Register Target Group CREATION SUCCEEDED: %s", rTargetResp)
 	return nil
+
 }
