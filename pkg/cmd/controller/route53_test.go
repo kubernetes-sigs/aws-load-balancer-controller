fff9a2a299f19c0704a850fe3dbb76bf5df1fa3f
Kraig Amador <Kraig.Amador@ticketmaster.com>
2017-03-10 11:54:42 -0800

    Working with unit tests

--- pkg/cmd/controller/route53_test.go
+++ pkg/cmd/controller/route53_test.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	"testing"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/route53"
 	"github.com/aws/aws-sdk-go/service/route53/route53iface"
@@ -17,131 +15,131 @@ const hostname = "2048.nonprod-tmaws.io"
 const zone = "nonprod-tmaws.io"
 const zoneID = "Z3QX9G7OLI3M7W"
 
-func TestLookupRecord(t *testing.T) {
-	setup()
+// func TestLookupRecord(t *testing.T) {
+// 	setup()
 
-	var tests = []struct {
-		hostname                     string
-		pass                         bool
-		listHostedZonesByNameOutput  *route53.ListHostedZonesByNameOutput
-		listResourceRecordSetsOutput *route53.ListResourceRecordSetsOutput
-	}{
-		{hostname, true, goodListHostedZonesByNameOutput, goodListResourceRecordSetsOutput},
-		{hostname, false, goodListHostedZonesByNameOutput, emptyListResourceRecordSetsOutput},
-		{"", false, goodListHostedZonesByNameOutput, emptyListResourceRecordSetsOutput},
-	}
+// 	var tests = []struct {
+// 		hostname                     string
+// 		pass                         bool
+// 		listHostedZonesByNameOutput  *route53.ListHostedZonesByNameOutput
+// 		listResourceRecordSetsOutput *route53.ListResourceRecordSetsOutput
+// 	}{
+// 		{hostname, true, goodListHostedZonesByNameOutput, goodListResourceRecordSetsOutput},
+// 		{hostname, false, goodListHostedZonesByNameOutput, emptyListResourceRecordSetsOutput},
+// 		{"", false, goodListHostedZonesByNameOutput, emptyListResourceRecordSetsOutput},
+// 	}
 
-	for _, tt := range tests {
-		r53responses["ListHostedZonesByName"] = tt.listHostedZonesByNameOutput
-		r53responses["ListResourceRecordSets"] = tt.listResourceRecordSetsOutput
+// 	for _, tt := range tests {
+// 		r53responses["ListHostedZonesByName"] = tt.listHostedZonesByNameOutput
+// 		r53responses["ListResourceRecordSets"] = tt.listResourceRecordSetsOutput
 
-		record, err := r53.lookupRecord(tt.hostname)
-		if tt.pass == false && err != nil {
-			continue
-		}
-		if err != nil && tt.pass {
-			t.Errorf("lookupRecord(%v): expected %v, got error: %s", tt.hostname, tt.hostname, err)
-		}
-		if err == nil && !tt.pass {
-			t.Errorf("lookupRecord(%v): expected %v, did not get error", tt.hostname, tt.pass)
-		}
-		if *record.Name != hostname && *record.Name != hostname+"." {
-			t.Errorf("lookupRecord(%v): expected %v, actual %s", tt.hostname, tt.hostname, *record.Name)
-		}
-	}
-}
+// 		record, err := r53.lookupRecord(tt.hostname)
+// 		if tt.pass == false && err != nil {
+// 			continue
+// 		}
+// 		if err != nil && tt.pass {
+// 			t.Errorf("lookupRecord(%v): expected %v, got error: %s", tt.hostname, tt.hostname, err)
+// 		}
+// 		if err == nil && !tt.pass {
+// 			t.Errorf("lookupRecord(%v): expected %v, did not get error", tt.hostname, tt.pass)
+// 		}
+// 		if *record.Name != hostname && *record.Name != hostname+"." {
+// 			t.Errorf("lookupRecord(%v): expected %v, actual %s", tt.hostname, tt.hostname, *record.Name)
+// 		}
+// 	}
+// }
 
-func TestGetDomain(t *testing.T) {
-	setup()
+// func TestGetDomain(t *testing.T) {
+// 	setup()
 
-	var tests = []struct {
-		hostname string
-		domain   string
-		pass     bool
-	}{
-		{"2048.domain.io", "domain.io", true},
-		{"alpha.beta.domain.io", "domain.io", true},
-		{"", "", false},
-	}
+// 	var tests = []struct {
+// 		hostname string
+// 		domain   string
+// 		pass     bool
+// 	}{
+// 		{"2048.domain.io", "domain.io", true},
+// 		{"alpha.beta.domain.io", "domain.io", true},
+// 		{"", "", false},
+// 	}
 
-	for _, tt := range tests {
-		actual, err := r53.getDomain(tt.hostname)
-		if tt.pass == false && err != nil {
-			continue
-		}
-		if err != nil {
-			t.Errorf("getDomain(%s): expected %s, got error: %s", tt.hostname, tt.domain, err)
-		}
-		if actual != tt.domain {
-			t.Errorf("getDomain(%s): expected %s, actual %s", tt.hostname, tt.domain, actual)
-		}
-	}
-}
+// 	for _, tt := range tests {
+// 		actual, err := r53.getDomain(tt.hostname)
+// 		if tt.pass == false && err != nil {
+// 			continue
+// 		}
+// 		if err != nil {
+// 			t.Errorf("getDomain(%s): expected %s, got error: %s", tt.hostname, tt.domain, err)
+// 		}
+// 		if actual != tt.domain {
+// 			t.Errorf("getDomain(%s): expected %s, actual %s", tt.hostname, tt.domain, actual)
+// 		}
+// 	}
+// }
 
-func TestGetZone(t *testing.T) {
-	setup()
+// func TestGetZone(t *testing.T) {
+// 	setup()
 
-	var tests = []struct {
-		zone                        string
-		pass                        bool
-		listHostedZonesByNameOutput *route53.ListHostedZonesByNameOutput
-	}{
-		{zone, true, goodListHostedZonesByNameOutput},
-		{zone + ".bad", false, goodListHostedZonesByNameOutput},
-		{zone, false, emptyListHostedZonesByNameOutput},
-	}
+// 	var tests = []struct {
+// 		zone                        string
+// 		pass                        bool
+// 		listHostedZonesByNameOutput *route53.ListHostedZonesByNameOutput
+// 	}{
+// 		{zone, true, goodListHostedZonesByNameOutput},
+// 		{zone + ".bad", false, goodListHostedZonesByNameOutput},
+// 		{zone, false, emptyListHostedZonesByNameOutput},
+// 	}
 
-	for _, tt := range tests {
-		r53responses["ListHostedZonesByName"] = tt.listHostedZonesByNameOutput
+// 	for _, tt := range tests {
+// 		r53responses["ListHostedZonesByName"] = tt.listHostedZonesByNameOutput
 
-		actual, err := r53.getZoneID(tt.zone)
-		if err != nil && tt.pass == false {
-			continue
-		}
-		if err != nil {
-			t.Errorf("getZoneID(%s): expected %v, got error: %s", tt.zone, tt.pass, err)
-		}
-		if *actual.Name != tt.zone && *actual.Name != tt.zone+"." {
-			t.Errorf("getZoneID(%s): expected %v, actual %v", tt.zone, tt.pass, *actual.Name)
-		}
-	}
-}
+// 		actual, err := r53.getZoneID(tt.zone)
+// 		if err != nil && tt.pass == false {
+// 			continue
+// 		}
+// 		if err != nil {
+// 			t.Errorf("getZoneID(%s): expected %v, got error: %s", tt.zone, tt.pass, err)
+// 		}
+// 		if *actual.Name != tt.zone && *actual.Name != tt.zone+"." {
+// 			t.Errorf("getZoneID(%s): expected %v, actual %v", tt.zone, tt.pass, *actual.Name)
+// 		}
+// 	}
+// }
 
-func TestModifyRecord(t *testing.T) {
-	setup()
+// func TestModifyRecord(t *testing.T) {
+// 	setup()
 
-	var tests = []struct {
-		hostname                       string
-		target                         string
-		targetZoneID                   string
-		action                         string
-		pass                           bool
-		changeResourceRecordSetsOutput *route53.ChangeResourceRecordSetsOutput
-	}{
-		{hostname, "target.com", "ZZZZZZZZZZZZZZ", "UPSERT", true, goodChangeResourceRecordSetsOutput},
-	}
+// 	var tests = []struct {
+// 		hostname                       string
+// 		target                         string
+// 		targetZoneID                   string
+// 		action                         string
+// 		pass                           bool
+// 		changeResourceRecordSetsOutput *route53.ChangeResourceRecordSetsOutput
+// 	}{
+// 		{hostname, "target.com", "ZZZZZZZZZZZZZZ", "UPSERT", true, goodChangeResourceRecordSetsOutput},
+// 	}
 
-	for _, tt := range tests {
-		r53responses["ListHostedZonesByName"] = goodListHostedZonesByNameOutput
-		r53responses["ChangeResourceRecordSets"] = tt.changeResourceRecordSetsOutput
-		alb := &albIngress{
-			hostname:              tt.hostname,
-			loadBalancerDNSName:   tt.target,
-			canonicalHostedZoneId: tt.targetZoneID,
-		}
+// 	for _, tt := range tests {
+// 		r53responses["ListHostedZonesByName"] = goodListHostedZonesByNameOutput
+// 		r53responses["ChangeResourceRecordSets"] = tt.changeResourceRecordSetsOutput
+// 		alb := &albIngress{
+// 			hostname:              tt.hostname,
+// 			loadBalancerDNSName:   tt.target,
+// 			canonicalHostedZoneId: tt.targetZoneID,
+// 		}
 
-		err := r53.modifyRecord(alb, tt.action)
-		if tt.pass == false && err != nil {
-			continue
-		}
-		if tt.pass == false && err == nil {
-			t.Errorf("modifyRecord(%v, %v) expected %v, did not error", alb, tt.action, tt.pass)
-		}
-		if err != nil && tt.pass {
-			t.Errorf("modifyRecord(%v, %v) expected %v, got error: %s", alb, tt.action, tt.pass, err)
-		}
-	}
-}
+// 		err := r53.modifyRecord(alb, tt.action)
+// 		if tt.pass == false && err != nil {
+// 			continue
+// 		}
+// 		if tt.pass == false && err == nil {
+// 			t.Errorf("modifyRecord(%v, %v) expected %v, did not error", alb, tt.action, tt.pass)
+// 		}
+// 		if err != nil && tt.pass {
+// 			t.Errorf("modifyRecord(%v, %v) expected %v, got error: %s", alb, tt.action, tt.pass, err)
+// 		}
+// 	}
+// }
 
 var (
 	goodListResourceRecordSetsOutput  *route53.ListResourceRecordSetsOutput
@@ -155,11 +153,7 @@ var (
 	goodChangeResourceRecordSetsOutput *route53.ChangeResourceRecordSetsOutput
 )
 
-func setupReal() {
-	r53 = newRoute53(nil)
-}
-
-func setup() {
+func setupRoute53() {
 	r53 = newRoute53(nil)
 	r53.svc = &mockRoute53Client{}
 	r53responses = make(map[string]interface{})
