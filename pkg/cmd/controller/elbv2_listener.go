6f67358d9544b139c71cbcac47bdbdc49c26c02f
Kraig Amador <Kraig.Amador@ticketmaster.com>
2017-03-21 18:32:34 -0600

    Our Listener now contains the CurrentListener and DesiredListener pointers and uses those for management of the Listener

--- pkg/cmd/controller/elbv2_listener.go
+++ pkg/cmd/controller/elbv2_listener.go
@@ -1,34 +1,43 @@
 package controller
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/awsutil"
 	"github.com/aws/aws-sdk-go/service/elbv2"
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Listener struct {
-	port         *int64
-	protocol     *string
-	deleted      bool
-	certificates []*elbv2.Certificate
-	Listener     *elbv2.Listener
-	Rules        []*elbv2.Rule
+	CurrentListener *elbv2.Listener
+	DesiredListener *elbv2.Listener
+	Rules           []*elbv2.Rule
 }
 
 type Listeners []*Listener
 
-func NewListener(a *albIngress) *Listener {
-	listener := &Listener{
-		port:     aws.Int64(80),
-		protocol: aws.String("HTTP"),
+func NewListener(annotations *annotationsT) *elbv2.Listener {
+	listener := &elbv2.Listener{
+		Port:     aws.Int64(80),
+		Protocol: aws.String("HTTP"),
 	}
-	listener.applyAnnotations(a)
+
+	if annotations.certificateArn != nil {
+		listener.Certificates = []*elbv2.Certificate{
+			&elbv2.Certificate{
+				CertificateArn: annotations.certificateArn,
+			},
+		}
+		listener.Protocol = aws.String("HTTPS")
+		listener.Port = aws.Int64(443)
+	}
+
+	if annotations.port != nil {
+		listener.Port = annotations.port
+	}
 
 	return listener
 }
@@ -37,15 +46,12 @@ func NewListener(a *albIngress) *Listener {
 func (l *Listener) create(a *albIngress, lb *LoadBalancer, tg *TargetGroup) error {
 	// Debug logger to introspect CreateListener request
 	glog.Infof("%s: Create Listener for %s sent", a.Name(), *lb.hostname)
-	if noop {
-		return nil
-	}
 
 	createListenerInput := &elbv2.CreateListenerInput{
-		Certificates:    l.certificates,
+		Certificates:    l.DesiredListener.Certificates,
 		LoadBalancerArn: lb.LoadBalancer.LoadBalancerArn,
-		Protocol:        l.protocol,
-		Port:            l.port,
+		Protocol:        l.DesiredListener.Protocol,
+		Port:            l.DesiredListener.Port,
 		DefaultActions: []*elbv2.Action{
 			{
 				Type:           aws.String("forward"),
@@ -65,24 +71,25 @@ func (l *Listener) create(a *albIngress, lb *LoadBalancer, tg *TargetGroup) erro
 		return err
 	}
 
-	l.Listener = createListenerOutput.Listeners[0]
+	l.CurrentListener = createListenerOutput.Listeners[0]
 	return nil
 }
 
 // Modifies a listener
 func (l *Listener) modify(a *albIngress, lb *LoadBalancer, tg *TargetGroup) error {
-	if l.Listener == nil {
+	if l.CurrentListener == nil {
 		// not a modify, a create
-
 		return l.create(a, lb, tg)
 	}
 
-	newListener := NewListener(a)
-	if newListener.Hash() == l.Hash() {
+	// Create the DesiredListener
+	l.DesiredListener = NewListener(a.annotations)
+	if !l.Equals(l.DesiredListener) {
 		return nil
 	}
 
-	glog.Infof("%s: Modifying existing %s listener %s", a.Name(), *lb.id, *l.Listener.ListenerArn)
+	glog.Infof("%s: Modifying existing %s listener %s", a.Name(), *lb.id, *l.CurrentListener.ListenerArn)
+	glog.Infof("%s: Have %v, want %v", a.Name(), l.CurrentListener, l.DesiredListener)
 	glog.Infof("%s: NOT IMPLEMENTED!!!!", a.Name())
 
 	return nil
@@ -91,50 +98,34 @@ func (l *Listener) modify(a *albIngress, lb *LoadBalancer, tg *TargetGroup) erro
 // Deletes a Listener from an existing ALB in AWS.
 func (l *Listener) delete(a *albIngress) error {
 	deleteListenerInput := &elbv2.DeleteListenerInput{
-		ListenerArn: l.Listener.ListenerArn,
+		ListenerArn: l.CurrentListener.ListenerArn,
 	}
 
 	// Debug logger to introspect DeleteListener request
-	glog.Infof("%s: Delete listener %s", a.Name(), *l.Listener.ListenerArn)
-	if !noop {
-		_, err := elbv2svc.svc.DeleteListener(deleteListenerInput)
-		if err != nil {
-			AWSErrorCount.With(prometheus.Labels{"service": "ELBV2", "request": "DeleteListener"}).Add(float64(1))
-			return err
-		}
+	glog.Infof("%s: Delete listener %s", a.Name(), *l.CurrentListener.ListenerArn)
+	_, err := elbv2svc.svc.DeleteListener(deleteListenerInput)
+	if err != nil {
+		AWSErrorCount.With(prometheus.Labels{"service": "ELBV2", "request": "DeleteListener"}).Add(float64(1))
+		return err
 	}
 	return nil
 }
 
-func (l *Listener) applyAnnotations(a *albIngress) {
+func (l *Listener) Equals(target *elbv2.Listener) bool {
 	switch {
-	case a.annotations.certificateArn != nil:
-		l.certificates = []*elbv2.Certificate{
-			&elbv2.Certificate{
-				CertificateArn: a.annotations.certificateArn,
-			},
-		}
-		l.protocol = aws.String("HTTPS")
-		l.port = aws.Int64(443)
-	case a.annotations.port != nil:
-		l.port = a.annotations.port
+	case *l.CurrentListener.Port != *target.Port:
+		return false
+	case *l.CurrentListener.Protocol != *target.Protocol:
+		return false
+	case awsutil.Prettify(l.CurrentListener.Certificates) != awsutil.Prettify(target.Certificates):
+		return false
 	}
-}
-
-func (l *Listener) Hash() string {
-	hasher := md5.New()
-
-	hasher.Write([]byte(fmt.Sprintf("%v%v", *l.port, *l.protocol)))
-	for _, certificate := range l.Listener.Certificates {
-		hasher.Write([]byte(*certificate.CertificateArn))
-	}
-	output := hex.EncodeToString(hasher.Sum(nil))
-	return output
+	return true
 }
 
 func (l Listeners) find(listener *Listener) int {
 	for p, v := range l {
-		if listener.Hash() == v.Hash() {
+		if listener.Equals(v.CurrentListener) {
 			return p
 		}
 	}
@@ -146,7 +137,7 @@ func (l Listeners) purgeTargetGroupArn(a *albIngress, arn *string) Listeners {
 	var listeners Listeners
 	for _, listener := range l {
 		// TODO: do we ever have more default actions?
-		if *listener.Listener.DefaultActions[0].TargetGroupArn != *arn {
+		if *listener.CurrentListener.DefaultActions[0].TargetGroupArn != *arn {
 			listeners = append(listeners, listener)
 		}
 	}
@@ -157,7 +148,7 @@ func (l Listeners) modify(a *albIngress, lb *LoadBalancer) error {
 	var li Listeners
 	for _, targetGroup := range lb.TargetGroups {
 		for _, listener := range lb.Listeners {
-			if listener.deleted {
+			if listener.DesiredListener == nil {
 				listener.delete(a)
 				continue
 			}
@@ -177,7 +168,7 @@ func (l Listeners) delete(a *albIngress) error {
 		if err := listener.delete(a); err != nil {
 			glog.Infof("%s: Unable to delete listener %s: %s",
 				a.Name(),
-				*listener.Listener.ListenerArn,
+				*listener.CurrentListener.ListenerArn,
 				err)
 		}
 	}
