86ffb99833ce7222133dde8364ba9feeb0fc0e92
Kraig Amador <Kraig.Amador@ticketmaster.com>
2017-03-24 17:02:37 -0600

    tried to clarify whats happening with r53 in newalbingress and commented out a bunch of code we will not need once we are done with SyncState

--- pkg/cmd/controller/elbv2_targetgroups.go
+++ pkg/cmd/controller/elbv2_targetgroups.go
@@ -1,12 +1,5 @@
 package controller
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/golang/glog"
-)
-
 type TargetGroups []*TargetGroup
 
 func (t TargetGroups) find(tg *TargetGroup) int {
@@ -29,60 +22,60 @@ func (t TargetGroups) SyncState() TargetGroups {
 	return targetgroups
 }
 
-func (t TargetGroups) modify(a *ALBIngress, lb *LoadBalancer) error {
-	var tg TargetGroups
+// func (t TargetGroups) modify(a *ALBIngress, lb *LoadBalancer) error {
+// 	var tg TargetGroups
 
-	for _, targetGroup := range lb.TargetGroups {
-		if targetGroup.DesiredTargetGroup == nil {
-			lb.Listeners = lb.Listeners.purgeTargetGroupArn(a, targetGroup.CurrentTargetGroup.TargetGroupArn)
-			targetGroup.delete()
-			continue
-		}
+// 	for _, targetGroup := range lb.TargetGroups {
+// 		if targetGroup.DesiredTargetGroup == nil {
+// 			lb.Listeners = lb.Listeners.purgeTargetGroupArn(a, targetGroup.CurrentTargetGroup.TargetGroupArn)
+// 			targetGroup.delete()
+// 			continue
+// 		}
 
-		if targetGroup.CurrentTargetGroup == nil {
-			targetGroup.create(a, lb)
-			continue
-		}
+// 		if targetGroup.CurrentTargetGroup == nil {
+// 			targetGroup.create(a, lb)
+// 			continue
+// 		}
 
-		if targetGroup.needsModification() {
-			err := targetGroup.modify(a, lb)
-			if err != nil {
-				glog.Errorf("%s: Error when modifying target group %s: %s", a.Name(), *targetGroup.id, err)
-				tg = append(tg, targetGroup)
-				continue
-			}
+// 		if targetGroup.needsModification() {
+// 			err := targetGroup.modify(a, lb)
+// 			if err != nil {
+// 				glog.Errorf("%s: Error when modifying target group %s: %s", a.Name(), *targetGroup.id, err)
+// 				tg = append(tg, targetGroup)
+// 				continue
+// 			}
 
-			for {
-				glog.Infof("%s: Waiting for target group %s to be online", a.Name(), *targetGroup.id)
-				if targetGroup.online(a) == true {
-					break
-				}
-				time.Sleep(5 * time.Second)
-			}
-		}
+// 			for {
+// 				glog.Infof("%s: Waiting for target group %s to be online", a.Name(), *targetGroup.id)
+// 				if targetGroup.online(a) == true {
+// 					break
+// 				}
+// 				time.Sleep(5 * time.Second)
+// 			}
+// 		}
 
-		tg = append(tg, targetGroup)
-	}
+// 		tg = append(tg, targetGroup)
+// 	}
 
-	lb.TargetGroups = tg
-	return nil
-}
+// 	lb.TargetGroups = tg
+// 	return nil
+// }
 
-func (t TargetGroups) delete() error {
-	errors := false
-	for _, targetGroup := range t {
-		if err := targetGroup.delete(); err != nil {
-			glog.Infof("Unable to delete target group %s: %s",
-				*targetGroup.id,
-				err)
-			errors = true
-		}
-	}
-	if errors {
-		return fmt.Errorf("There were errors deleting target groups")
-	}
-	return nil
-}
+// func (t TargetGroups) delete() error {
+// 	errors := false
+// 	for _, targetGroup := range t {
+// 		if err := targetGroup.delete(); err != nil {
+// 			glog.Infof("Unable to delete target group %s: %s",
+// 				*targetGroup.id,
+// 				err)
+// 			errors = true
+// 		}
+// 	}
+// 	if errors {
+// 		return fmt.Errorf("There were errors deleting target groups")
+// 	}
+// 	return nil
+// }
 
 func (t TargetGroups) StripDesiredState() {
 	for _, targetgroup := range t {
