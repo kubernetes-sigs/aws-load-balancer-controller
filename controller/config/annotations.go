b61e86d4882b0a1389e3b402967e2d6c240b5730
joshrosso <joshrosso@gmail.com>
2017-04-10 14:18:07 -0600

    Validate subnets after annotation parsing
    - Ensures subnets are valid by retrieving VPC ID correlated to them.
    - Stores VPC ID in struct for later LoadBalancer creation.
    - Resolves #24.
    
    wip 3

--- controller/config/annotations.go
+++ controller/config/annotations.go
@@ -43,6 +43,7 @@ type Annotations struct {
 	Subnets         util.Subnets
 	SuccessCodes    *string
 	Tags            []*elbv2.Tag
+	VPCID           *string
 }
 
 // ListenerPort represents a listener defined in an ingress annotation. Specifically, it represents a
@@ -58,8 +59,6 @@ type ListenerPort struct {
 // annotations are also cached, meaning there will be no reattempt to parse annotations until the
 // cache expires or the value(s) change.
 func ParseAnnotations(annotations map[string]string) (*Annotations, error) {
-	resp := &Annotations{}
-
 	sortedAnnotations := util.SortedMap(annotations)
 	cacheKey := "annotations " + awsutil.Prettify(sortedAnnotations)
 
@@ -84,6 +83,7 @@ func ParseAnnotations(annotations map[string]string) (*Annotations, error) {
 		cache.Set(cacheKey, "error", 1*time.Hour)
 		return nil, err
 	}
+
 	securitygroups, err := parseSecurityGroups(annotations[securityGroupsKey])
 	if err != nil {
 		cache.Set(cacheKey, "error", 1*time.Hour)
@@ -101,7 +101,7 @@ func ParseAnnotations(annotations map[string]string) (*Annotations, error) {
 		return nil, err
 	}
 
-	resp = &Annotations{
+	a := &Annotations{
 		BackendProtocol: aws.String(annotations[backendProtocolKey]),
 		Ports:           ports,
 		Subnets:         subnets,
@@ -113,10 +113,15 @@ func ParseAnnotations(annotations map[string]string) (*Annotations, error) {
 	}
 
 	if cert, ok := annotations[certificateArnKey]; ok {
-		resp.CertificateArn = aws.String(cert)
+		a.CertificateArn = aws.String(cert)
 	}
 
-	return resp, nil
+	if err := a.resolveVPC(); err != nil {
+		cache.Set(cacheKey, "error", 1*time.Hour)
+		return nil, err
+	}
+
+	return a, nil
 }
 
 // parsePorts takes a JSON array describing what ports and protocols should be used. When the JSON
@@ -233,19 +238,6 @@ func parseSubnets(s string) (out util.Subnets, err error) {
 		awsutil.AWSCache.With(prometheus.Labels{"cache": "subnets", "action": "miss"}).Add(float64(1))
 
 		names = append(names, subnet)
-	}
-
-	// Verify Subnets resolved from annotation exist.
-	if len(out) > 0 {
-		in := ec2.DescribeSubnetsInput{
-			SubnetIds: out,
-		}
-		_, err := awsutil.Ec2svc.DescribeSubnets(in)
-		if err != nil {
-			log.Errorf("Subnets specified were invalid. subnets: %s | Error: %s.", "controller",
-				s, err.Error())
-			return nil, err
-		}
 	}
 
 	if len(names) > 0 {
---
+++ controller/config/validation.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/coreos/alb-ingress-controller/awsutil"
+)
+
+// resolveVPC attempt to resolve a VPC based on the provided subnets. This also acts as a way to
+// validate provided subnets exist.
+func (a *Annotations) resolveVPC() error {
+	VPCID, err := awsutil.Ec2svc.GetVPCID(a.Subnets)
+	if err != nil {
+		return fmt.Errorf("Subnets %s were invalid. Could not resolve to a VPC.")
+	}
+	a.VPCID = VPCID
+	return nil
+}
