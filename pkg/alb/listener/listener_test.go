84fc748be2ee64f202a209d341c0016bfcb5ea9a
joshrosso <joshrosso@gmail.com>
2017-08-21 07:10:00 -0600

    Add listener create reconcile test

--- pkg/alb/listener/listener_test.go
+++ pkg/alb/listener/listener_test.go
@@ -4,23 +4,32 @@ import (
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
+	awselb "github.com/aws/aws-sdk-go/service/elbv2"
 	"github.com/coreos/alb-ingress-controller/pkg/aws/elbv2"
 	"github.com/coreos/alb-ingress-controller/pkg/util/log"
+	"github.com/coreos/alb-ingress-controller/pkg/util/types"
+)
+
+const (
+	newARN   = "arn1"
+	newTg    = "tg1"
+	newPort  = 8080
+	newProto = "HTTP"
 )
 
 var (
-	logger *log.Logger
+	logr *log.Logger
 )
 
 func init() {
-	logger = log.New("test")
+	logr = log.New("test")
 }
 
 func TestNewHTTPListener(t *testing.T) {
-	desiredPort := int64(8080)
+	desiredPort := int64(newPort)
 	o := &NewDesiredListenerOptions{
 		Port:   desiredPort,
-		Logger: logger,
+		Logger: logr,
 	}
 
 	l := NewDesiredListener(o)
@@ -45,7 +54,7 @@ func TestNewHTTPSListener(t *testing.T) {
 	o := &NewDesiredListenerOptions{
 		Port:           desiredPort,
 		CertificateArn: desiredCertArn,
-		Logger:         logger,
+		Logger:         logr,
 	}
 
 	l := NewDesiredListener(o)
@@ -68,24 +77,55 @@ func TestNewHTTPSListener(t *testing.T) {
 }
 
 type mockELBV2Client struct {
-	elbv2.ELBV2
+	elbv2.ELBV2API
 }
 
-/*func TestReconcileCreate(t *testing.T) {
+func (m mockELBV2Client) CreateListener(*awselb.CreateListenerInput) (*awselb.CreateListenerOutput, error) {
+	o := &awselb.CreateListenerOutput{
+		Listeners: []*awselb.Listener{
+			{
+				Port:        aws.Int64(newPort),
+				ListenerArn: aws.String(newARN),
+				Protocol:    aws.String(newProto),
+			}},
+	}
+	return o, nil
+}
 
+func mockEventf(a, b, c string, d ...interface{}) {
+}
+
+// TestReconcileCreate calls Reconcile on a mock Listener instance and assures creation is
+// attempted.
+func TestReconcileCreate(t *testing.T) {
 	elbv2.ELBV2svc = mockELBV2Client{}
 
-	tg :=
-		targetgroup.TargetGroup{
-			SvcName: "service1",
-		}
-	tgs := targetgroups.TargetGroups{
-
+	dl := &awselb.Listener{
+		Port:     aws.Int64(8080),
+		Protocol: aws.String("HTTP"),
+		DefaultActions: []*awselb.Action{{
+			Type:           aws.String("default"),
+			TargetGroupArn: aws.String(newTg),
+		}},
 	}
 
-	rOpts := ReconcileOptions{
+	l := Listener{
+		logger:  logr,
+		Desired: dl,
+	}
+
+	rOpts := &ReconcileOptions{
 		TargetGroups:    nil,
 		LoadBalancerArn: nil,
-		Eventf:          nil,
+		Eventf:          mockEventf,
 	}
-}*/
+
+	l.Reconcile(rOpts)
+
+	if *l.Current.ListenerArn != newARN {
+		t.Errorf("Listener arn not properly set. Actual: %s, Expected: %s", *l.Current.ListenerArn, newARN)
+	}
+	if types.DeepEqual(l.Desired, l.Current) {
+		t.Error("After creation, desired and current listeners did not match.")
+	}
+}
