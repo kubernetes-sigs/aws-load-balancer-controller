0d5265002bf5fe9624c4ba1950b718886df3d2e7
Gyuho Lee <leegyuho@amazon.com>
2018-09-03 17:26:58 -0700

    internal/aws/albwafregional: "ObserveAPIRequest" WAF API
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- internal/aws/albwafregional/wafregional.go
+++ internal/aws/albwafregional/wafregional.go
@@ -9,6 +9,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/wafregional"
 	"github.com/aws/aws-sdk-go/service/wafregional/wafregionaliface"
 	"github.com/kubernetes-sigs/aws-alb-ingress-controller/internal/aws/albcache"
+	"github.com/kubernetes-sigs/aws-alb-ingress-controller/internal/ingress/metric"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 // WAFRegionalsvc is a pointer to the awsutil WAFRegional service
@@ -17,12 +19,18 @@ var WAFRegionalsvc *WAFRegional
 // WAFRegional is our extension to AWS's WAFRegional.wafregional
 type WAFRegional struct {
 	wafregionaliface.WAFRegionalAPI
+	mc metric.Collector
 }
 
 // NewWAFRegional returns an WAFRegional based off of the provided aws.Config
-func NewWAFRegional(awsSession *session.Session) {
+func NewWAFRegional(awsSession *session.Session, mc metric.Collector) {
 	WAFRegionalsvc = &WAFRegional{
-		wafregional.New(awsSession),
+		WAFRegionalAPI: wafregional.New(awsSession),
+		mc:             mc,
+	}
+	if WAFRegionalsvc.mc == nil {
+		// prevent nil pointer panic
+		WAFRegionalsvc.mc = metric.DummyCollector{}
 	}
 }
 
@@ -36,9 +44,11 @@ func (a *WAFRegional) WebACLExists(webACLId *string) (bool, error) {
 		return v, nil
 	}
 
+	start := time.Now()
 	_, err := a.GetWebACL(&waf.GetWebACLInput{
 		WebACLId: webACLId,
 	})
+	a.mc.ObserveAPIRequest(prometheus.Labels{"operation": "GetWebACL"}, start)
 
 	if err != nil {
 		albcache.Set(cacheName, *webACLId, false, time.Minute*5)
@@ -59,13 +69,14 @@ func (a *WAFRegional) GetWebACLSummary(resourceArn *string) (*waf.WebACLSummary,
 		return v, nil
 	}
 
+	start := time.Now()
 	result, err := a.GetWebACLForResource(&wafregional.GetWebACLForResourceInput{
 		ResourceArn: aws.String(*resourceArn),
 	})
-
 	if err != nil {
 		return nil, err
 	}
+	a.mc.ObserveAPIRequest(prometheus.Labels{"operation": "GetWebACLForResource"}, start)
 
 	albcache.Set(cacheName, *resourceArn, result.WebACLSummary, time.Minute*5)
 	return result.WebACLSummary, nil
@@ -73,27 +84,29 @@ func (a *WAFRegional) GetWebACLSummary(resourceArn *string) (*waf.WebACLSummary,
 
 // Associate WAF ACL to resource.
 func (a *WAFRegional) Associate(resourceArn *string, webACLId *string) (*wafregional.AssociateWebACLOutput, error) {
+	start := time.Now()
 	result, err := a.AssociateWebACL(&wafregional.AssociateWebACLInput{
 		ResourceArn: resourceArn,
 		WebACLId:    webACLId,
 	})
-
 	if err != nil {
 		return nil, err
 	}
+	a.mc.ObserveAPIRequest(prometheus.Labels{"operation": "AssociateWebACL"}, start)
 
 	return result, nil
 }
 
 // Disassociate WAF ACL from resource.
 func (a *WAFRegional) Disassociate(resourceArn *string) (*wafregional.DisassociateWebACLOutput, error) {
+	start := time.Now()
 	result, err := a.DisassociateWebACL(&wafregional.DisassociateWebACLInput{
 		ResourceArn: resourceArn,
 	})
-
 	if err != nil {
 		return nil, err
 	}
+	a.mc.ObserveAPIRequest(prometheus.Labels{"operation": "DisassociateWebACL"}, start)
 
 	return result, nil
 }
