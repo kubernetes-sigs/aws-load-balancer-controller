3cf9be746bda1c5e1e0dc412165d28d34c2bc817
Gyuho Lee <leegyuho@amazon.com>
2018-09-03 17:22:26 -0700

    internal/aws/albacm,iam: "ObserveAPIRequest" ACM, IAM API
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- internal/aws/albiam/iam.go
+++ internal/aws/albiam/iam.go
@@ -1,9 +1,14 @@
 package albiam
 
 import (
+	"time"
+
+	"github.com/kubernetes-sigs/aws-alb-ingress-controller/internal/ingress/metric"
+
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/iam/iamiface"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 // IAMsvc is a pointer to the awsutil IAM service
@@ -12,12 +17,18 @@ var IAMsvc *IAM
 // IAM is our extension to AWS's IAM.iam
 type IAM struct {
 	iamiface.IAMAPI
+	mc metric.Collector
 }
 
 // NewIAM returns an IAM based off of the provided aws.Config
-func NewIAM(awsSession *session.Session) {
+func NewIAM(awsSession *session.Session, mc metric.Collector) {
 	IAMsvc = &IAM{
-		iam.New(awsSession),
+		IAMAPI: iam.New(awsSession),
+		mc:     mc,
+	}
+	if IAMsvc.mc == nil {
+		// prevent nil pointer panic
+		IAMsvc.mc = metric.DummyCollector{}
 	}
 }
 
@@ -27,9 +38,11 @@ func (i *IAM) Status() func() error {
 		in := &iam.ListServerCertificatesInput{}
 		in.SetMaxItems(1)
 
+		start := time.Now()
 		if _, err := i.ListServerCertificates(in); err != nil {
 			return err
 		}
+		i.mc.ObserveAPIRequest(prometheus.Labels{"operation": "ListServerCertificates"}, start)
 		return nil
 	}
 }
