37eb62027adf06f6965207e3b90de3885a3c3f6e
Kraig Amador <kraig.amador@ticketmaster.com>
2018-09-27 17:33:09 -0500

    Test attributes Reconcile

--- internal/alb/lb/attributes_test.go
+++ internal/alb/lb/attributes_test.go
@@ -1,12 +1,16 @@
 package lb
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/kubernetes-sigs/aws-alb-ingress-controller/mocks"
+	"github.com/stretchr/testify/assert"
 )
 
 func MustNewAttributes(a []*elbv2.LoadBalancerAttribute) *Attributes {
@@ -185,5 +189,131 @@ func Test_attributesChangeSet(t *testing.T) {
 		if !reflect.DeepEqual(tc.changeSet, changeSet) {
 			t.Errorf("%v: expected %v, actual %v", tc.name, tc.changeSet, changeSet)
 		}
+	}
+}
+
+type DescribeLoadBalancerAttributesCall struct {
+	LbArn  *string
+	Output *elbv2.DescribeLoadBalancerAttributesOutput
+	Err    error
+}
+
+type ModifyLoadBalancerAttributesCall struct {
+	Input  *elbv2.ModifyLoadBalancerAttributesInput
+	Output *elbv2.ModifyLoadBalancerAttributesOutput
+	Err    error
+}
+
+func defaultAttributes() []*elbv2.LoadBalancerAttribute {
+	return []*elbv2.LoadBalancerAttribute{
+		attr(DeletionProtectionEnabledString, "false"),
+		attr(AccessLogsS3EnabledString, "false"),
+		attr(AccessLogsS3BucketString, ""),
+		attr(AccessLogsS3PrefixString, ""),
+		attr(IdleTimeoutTimeoutSecondsString, "60"),
+		attr(RoutingHTTP2EnabledString, "true"),
+	}
+}
+
+func newattr(lbArn string, attrs []*elbv2.LoadBalancerAttribute) *Attributes {
+	a := MustNewAttributes(attrs)
+	a.LbArn = lbArn
+	return a
+}
+
+func TestReconcile(t *testing.T) {
+	for _, tc := range []struct {
+		Name                               string
+		Attributes                         *Attributes
+		DescribeLoadBalancerAttributesCall *DescribeLoadBalancerAttributesCall
+		ModifyLoadBalancerAttributesCall   *ModifyLoadBalancerAttributesCall
+		ExpectedError                      error
+	}{
+		{
+			Name:       "Load Balancer doesn't exist",
+			Attributes: &Attributes{},
+			DescribeLoadBalancerAttributesCall: &DescribeLoadBalancerAttributesCall{
+				LbArn:  aws.String(""),
+				Output: nil,
+				Err:    fmt.Errorf("ERROR STRING"),
+			},
+			ExpectedError: errors.New("failed to retrieve attributes from ELBV2 in AWS: ERROR STRING"),
+		},
+		{
+			Name:       "default attribute set",
+			Attributes: newattr("arn", nil),
+			DescribeLoadBalancerAttributesCall: &DescribeLoadBalancerAttributesCall{
+				LbArn:  aws.String("arn"),
+				Output: &elbv2.DescribeLoadBalancerAttributesOutput{Attributes: defaultAttributes()},
+				Err:    nil,
+			},
+			ModifyLoadBalancerAttributesCall: nil,
+			ExpectedError:                    nil,
+		},
+		{
+			Name:       "start with default attribute set, change to timeout to 120s",
+			Attributes: newattr("arn", []*elbv2.LoadBalancerAttribute{attr(IdleTimeoutTimeoutSecondsString, "120")}),
+			DescribeLoadBalancerAttributesCall: &DescribeLoadBalancerAttributesCall{
+				LbArn:  aws.String("arn"),
+				Output: &elbv2.DescribeLoadBalancerAttributesOutput{Attributes: defaultAttributes()},
+				Err:    nil,
+			},
+			ModifyLoadBalancerAttributesCall: &ModifyLoadBalancerAttributesCall{
+				Input: &elbv2.ModifyLoadBalancerAttributesInput{
+					LoadBalancerArn: aws.String("arn"),
+					Attributes: []*elbv2.LoadBalancerAttribute{
+						{
+							Key:   aws.String("idle_timeout.timeout_seconds"),
+							Value: aws.String("120"),
+						},
+					},
+				},
+				Err: nil,
+			},
+			ExpectedError: nil,
+		},
+		{
+			Name:       "start with default attribute set, API throws an error",
+			Attributes: newattr("arn", []*elbv2.LoadBalancerAttribute{attr(IdleTimeoutTimeoutSecondsString, "120")}),
+			DescribeLoadBalancerAttributesCall: &DescribeLoadBalancerAttributesCall{
+				LbArn:  aws.String("arn"),
+				Output: &elbv2.DescribeLoadBalancerAttributesOutput{Attributes: defaultAttributes()},
+				Err:    nil,
+			},
+			ModifyLoadBalancerAttributesCall: &ModifyLoadBalancerAttributesCall{
+				Input: &elbv2.ModifyLoadBalancerAttributesInput{
+					LoadBalancerArn: aws.String("arn"),
+					Attributes: []*elbv2.LoadBalancerAttribute{
+						{
+							Key:   aws.String("idle_timeout.timeout_seconds"),
+							Value: aws.String("120"),
+						},
+					},
+				},
+				Err: fmt.Errorf("Something unexpected happened"),
+			},
+			ExpectedError: errors.New("failed modifying attributes: Something unexpected happened"),
+		},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			elbv2svc := &mocks.ELBV2API{}
+			if tc.DescribeLoadBalancerAttributesCall != nil {
+				elbv2svc.On("DescribeLoadBalancerAttributes", &elbv2.DescribeLoadBalancerAttributesInput{LoadBalancerArn: tc.DescribeLoadBalancerAttributesCall.LbArn}).Return(tc.DescribeLoadBalancerAttributesCall.Output, tc.DescribeLoadBalancerAttributesCall.Err)
+			}
+
+			if tc.ModifyLoadBalancerAttributesCall != nil {
+				elbv2svc.On("ModifyLoadBalancerAttributes", tc.ModifyLoadBalancerAttributesCall.Input).Return(tc.ModifyLoadBalancerAttributesCall.Output, tc.ModifyLoadBalancerAttributesCall.Err)
+			}
+
+			controller := &attributesController{elbv2: elbv2svc}
+			err := controller.Reconcile(context.Background(), tc.Attributes)
+
+			if tc.ExpectedError != nil {
+				assert.Equal(t, tc.ExpectedError, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			elbv2svc.AssertExpectations(t)
+		})
 	}
 }
