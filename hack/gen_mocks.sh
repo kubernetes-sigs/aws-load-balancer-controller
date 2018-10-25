2b85ac624c3cd31055a8a76e3cf9f3760e9b30ab
Kraig Amador <kraig.amador@ticketmaster.com>
2018-10-25 10:58:52 -0700

    Updated mocks

--- hack/gen_mocks.sh
+++ hack/gen_mocks.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # This shell script is used to generate & update mock objects for testing
-mockery -name ELBV2API -dir ./internal/aws/albelbv2/
-mockery -name ResourceGroupsTaggingAPIAPI -dir ./internal/aws/albrgt/
+mockery -name CloudAPI -dir ./internal/aws/
 mockery -name Storer -dir ./internal/ingress/controller/store/ -inpkg
 
 mockery -name Controller -dir ./internal/alb/ls/ -inpkg
