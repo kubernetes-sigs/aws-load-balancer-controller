12b20eedc2fdf872316ecf6225b6337dc4e03a89
Yang <yyyng@amazon.com>
2018-11-02 13:19:26 -0700

    update

--- scripts/ci_unit_test.sh
+++ scripts/ci_unit_test.sh
@@ -2,11 +2,6 @@ COVER_PROFILE=profile.cov
 
 report_coverage() {
     go get github.com/mattn/goveralls
-    echo ${BUILD_ID}
-    echo ${PULL_NUMBER}
-    echo ${PROW_JOB_ID}
-    echo yyyng
-
     BUILD_NUMBER=${BUILD_ID}  PULL_REQUEST_NUMBER=${PULL_NUMBER} $(go env GOBIN)/goveralls \
            -coverprofile=$COVER_PROFILE \
            -service=prow \
