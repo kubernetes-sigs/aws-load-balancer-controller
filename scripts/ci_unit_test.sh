f62b6f826dbf5173ca72264d9df1c1e2a3eeba90
Yang <yyyng@amazon.com>
2018-11-02 13:25:08 -0700

    change coverall version

--- scripts/ci_unit_test.sh
+++ scripts/ci_unit_test.sh
@@ -2,7 +2,7 @@ COVER_PROFILE=profile.cov
 
 report_coverage() {
     go get github.com/mattn/goveralls
-    BUILD_NUMBER=${BUILD_ID}  PULL_REQUEST_NUMBER=${PULL_NUMBER} $(go env GOBIN)/goveralls \
+    CIRCLE_BUILD_NUM=${BUILD_ID}  CIRCLE_PR_NUMBER=${PULL_NUMBER} $(go env GOBIN)/goveralls \
            -coverprofile=$COVER_PROFILE \
            -service=prow \
            -repotoken $(cat /etc/coveralls-token/k8s-aws-alb-ingress-coveralls-token) \
