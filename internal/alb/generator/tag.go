55f9a022467b814d6f4741c1225b45b6af37e743
Yang Yang <yyyng@amazon.com>
2019-09-12 16:44:38 -0700

    tag aws resources with v2 style tags to enable migration to v2

--- internal/alb/generator/tag.go
+++ internal/alb/generator/tag.go
@@ -1,6 +1,8 @@
 package generator
 
 import (
+	"fmt"
+
 	"github.com/kubernetes-sigs/aws-alb-ingress-controller/internal/alb/lb"
 	"github.com/kubernetes-sigs/aws-alb-ingress-controller/internal/alb/sg"
 	"github.com/kubernetes-sigs/aws-alb-ingress-controller/internal/alb/tg"
@@ -15,6 +17,16 @@ const (
 	TagKeyServicePort = "kubernetes.io/service-port"
 )
 
+// Additional Tags used to be forward-compatible with V2 version.
+const (
+	V2TagKeyClusterID  = "ingress.k8s.aws/cluster"
+	V2TagKeyStackID    = "ingress.k8s.aws/stack"
+	V2TagKeyResourceID = "ingress.k8s.aws/resource"
+
+	V2ResourceIDLoadBalancer           = "LoadBalancer"
+	V2ResourceIDManagedLBSecurityGroup = "ManagedLBSecurityGroup"
+)
+
 var _ tg.TagGenerator = (*TagGenerator)(nil)
 var _ lb.TagGenerator = (*TagGenerator)(nil)
 var _ sg.TagGenerator = (*TagGenerator)(nil)
@@ -25,22 +37,29 @@ type TagGenerator struct {
 }
 
 func (gen *TagGenerator) TagLB(namespace string, ingressName string) map[string]string {
-	return gen.tagIngressResources(namespace, ingressName)
+	resTags := gen.tagIngressResources(namespace, ingressName)
+	resTags[V2TagKeyResourceID] = V2ResourceIDLoadBalancer
+	return resTags
 }
 
 func (gen *TagGenerator) TagTGGroup(namespace string, ingressName string) map[string]string {
 	return gen.tagIngressResources(namespace, ingressName)
 }
 
-func (gen *TagGenerator) TagTG(serviceName string, servicePort string) map[string]string {
-	return map[string]string{
+func (gen *TagGenerator) TagTG(namespace string, ingressName string, serviceName string, servicePort string) map[string]string {
+	resTags := map[string]string{
 		TagKeyServiceName: serviceName,
 		TagKeyServicePort: servicePort,
 	}
+	resID := gen.buildV2TargetGroupID(namespace, ingressName, serviceName, servicePort)
+	resTags[V2TagKeyResourceID] = resID
+	return resTags
 }
 
 func (gen *TagGenerator) TagLBSG(namespace string, ingressName string) map[string]string {
-	return gen.tagSGs(namespace, ingressName)
+	resTags := gen.tagSGs(namespace, ingressName)
+	resTags[V2TagKeyResourceID] = V2ResourceIDManagedLBSecurityGroup
+	return resTags
 }
 
 func (gen *TagGenerator) TagInstanceSG(namespace string, ingressName string) map[string]string {
@@ -55,6 +74,11 @@ func (gen *TagGenerator) tagIngressResources(namespace string, ingressName strin
 	m["kubernetes.io/cluster/"+gen.ClusterName] = "owned"
 	m[TagKeyNamespace] = namespace
 	m[TagKeyIngressName] = ingressName
+
+	v2Tags := gen.tagIngressResourcesV2(namespace, ingressName)
+	for label, value := range v2Tags {
+		m[label] = value
+	}
 	return m
 }
 
@@ -76,5 +100,29 @@ func (gen *TagGenerator) tagSGs(namespace string, ingressName string) map[string
 
 	m[TagKeyNamespace] = namespace
 	m[TagKeyIngressName] = ingressName
+
+	v2Tags := gen.tagIngressResourcesV2(namespace, ingressName)
+	for label, value := range v2Tags {
+		m[label] = value
+	}
+
 	return m
+}
+
+func (gen *TagGenerator) tagIngressResourcesV2(namespace string, ingressName string) map[string]string {
+	m := make(map[string]string)
+	stackID := gen.buildV2StackID(namespace, ingressName)
+	m[V2TagKeyClusterID] = gen.ClusterName
+	m[V2TagKeyStackID] = stackID
+	return m
+}
+
+// buildV2StackID returns the stack ID that compatible with V2 version.
+func (gen *TagGenerator) buildV2StackID(namespace string, ingressName string) string {
+	return fmt.Sprintf("%s/%s", namespace, ingressName)
+}
+
+// buildV2TargetGroupID returns the targetGroup ResourceID that compatible with V2 version.
+func (gen *TagGenerator) buildV2TargetGroupID(namespace string, ingressName string, serviceName string, servicePort string) string {
+	return fmt.Sprintf("%s/%s-%s:%s", namespace, ingressName, serviceName, servicePort)
 }
