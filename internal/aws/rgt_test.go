25b97055cb637fbb727220027730b73f2674e296
Li Chen <mail@chen.li>
2019-12-19 11:47:51 -0500

    Rewrite GetClusterSubnets() using EC2 specific API

--- internal/aws/rgt_test.go
+++ internal/aws/rgt_test.go
@@ -7,10 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/request"
-	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi"
 	"github.com/kubernetes-sigs/aws-alb-ingress-controller/mocks"
-	util "github.com/kubernetes-sigs/aws-alb-ingress-controller/pkg/util/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -55,132 +53,6 @@ func TestCloud_UntagResourcesWithContext(t *testing.T) {
 		assert.Equal(t, b, e)
 		svc.AssertExpectations(t)
 	})
-}
-
-func tag(k, v string) *resourcegroupstaggingapi.Tag {
-	return &resourcegroupstaggingapi.Tag{Key: String(k), Value: String(v)}
-}
-
-func tags(t ...*resourcegroupstaggingapi.Tag) []*resourcegroupstaggingapi.Tag {
-	return t
-}
-
-func ec2Tag(k, v string) *ec2.Tag {
-	return &ec2.Tag{Key: aws.String(k), Value: aws.String(v)}
-}
-
-func TestCloud_GetClusterSubnets(t *testing.T) {
-	clusterName := "clusterName"
-	paramSets := []*resourcegroupstaggingapi.GetResourcesInput{
-		{
-			ResourcesPerPage: aws.Int64(50),
-			ResourceTypeFilters: []*string{
-				aws.String("ec2"),
-			},
-			TagFilters: []*resourcegroupstaggingapi.TagFilter{
-				{
-					Key:    aws.String("kubernetes.io/role/internal-elb"),
-					Values: []*string{aws.String(""), aws.String("1")},
-				},
-				{
-					Key:    aws.String("kubernetes.io/cluster/" + clusterName),
-					Values: []*string{aws.String("owned"), aws.String("shared")},
-				},
-			},
-		},
-		{
-			ResourcesPerPage: aws.Int64(50),
-			ResourceTypeFilters: []*string{
-				aws.String("ec2"),
-			},
-			TagFilters: []*resourcegroupstaggingapi.TagFilter{
-				{
-					Key:    aws.String("kubernetes.io/role/elb"),
-					Values: []*string{aws.String(""), aws.String("1")},
-				},
-				{
-					Key:    aws.String("kubernetes.io/cluster/" + clusterName),
-					Values: []*string{aws.String("owned"), aws.String("shared")},
-				},
-			},
-		},
-	}
-	for _, tc := range []struct {
-		Name               string
-		GetResourcesOutput *resourcegroupstaggingapi.GetResourcesOutput
-		GetResourcesError  awserr.Error
-		Params             int
-		ExpectedResult     map[string]util.EC2Tags
-		ExpectedError      error
-	}{
-		{
-			Name:   "No subnets returned",
-			Params: 2,
-			GetResourcesOutput: &resourcegroupstaggingapi.GetResourcesOutput{
-				ResourceTagMappingList: []*resourcegroupstaggingapi.ResourceTagMapping{
-					{
-						ResourceARN: aws.String("arn1"),
-						Tags:        tags(tag("tag1", "val1")),
-					},
-				},
-			},
-			ExpectedResult: map[string]util.EC2Tags{},
-		},
-		{
-			Name:   "Three subnets returned",
-			Params: 2,
-			GetResourcesOutput: &resourcegroupstaggingapi.GetResourcesOutput{
-				ResourceTagMappingList: []*resourcegroupstaggingapi.ResourceTagMapping{
-					{
-						ResourceARN: aws.String("arn:aws:ec2:region:account-id:subnet/subnet-id1"),
-						Tags:        tags(tag("tag1", "val1")),
-					},
-					{
-						ResourceARN: aws.String("arn:aws:ec2:region:account-id:subnet/subnet-id2"),
-						Tags:        tags(tag("tag2", "val2")),
-					},
-					{
-						ResourceARN: aws.String("arn:aws:ec2:region:account-id:subnet/subnet-id3"),
-						Tags:        tags(tag("tag3", "val3")),
-					},
-				},
-			},
-			ExpectedResult: map[string]util.EC2Tags{
-				"arn:aws:ec2:region:account-id:subnet/subnet-id1": {ec2Tag("tag1", "val1")},
-				"arn:aws:ec2:region:account-id:subnet/subnet-id2": {ec2Tag("tag2", "val2")},
-				"arn:aws:ec2:region:account-id:subnet/subnet-id3": {ec2Tag("tag3", "val3")},
-			},
-		},
-		{
-			Name:              "API throws an error",
-			Params:            1,
-			GetResourcesError: awserr.New(request.ErrCodeResponseTimeout, "timeout", nil),
-			ExpectedError:     awserr.New(request.ErrCodeResponseTimeout, "timeout", nil),
-		},
-	} {
-		t.Run(tc.Name, func(t *testing.T) {
-			rgtsvc := &mocks.ResourceGroupsTaggingAPIAPI{}
-
-			for paramSet := 0; paramSet < tc.Params; paramSet++ {
-				rgtsvc.On("GetResourcesPages",
-					paramSets[paramSet],
-					mock.AnythingOfType("func(*resourcegroupstaggingapi.GetResourcesOutput, bool) bool"),
-				).Return(tc.GetResourcesError).Run(func(args mock.Arguments) {
-					arg := args.Get(1).(func(*resourcegroupstaggingapi.GetResourcesOutput, bool) bool)
-					arg(tc.GetResourcesOutput, false)
-				})
-			}
-
-			cloud := &Cloud{
-				clusterName: clusterName,
-				rgt:         rgtsvc,
-			}
-			subnets, err := cloud.GetClusterSubnets()
-			assert.Equal(t, tc.ExpectedResult, subnets)
-			assert.Equal(t, tc.ExpectedError, err)
-			rgtsvc.AssertExpectations(t)
-		})
-	}
 }
 
 func TestCloud_GetResourcesByFilters(t *testing.T) {
