bcbbc4bf5988ccae9732768d9077f1cd077471cc
M00nF1sh <yyyng@amazon.com>
2020-08-13 16:51:34 -0700

    add targets manager (#1342)
    * add targets manager
    
    * add UT for recordSuccessfulRegisterTargetsOperation/recordSuccessfulDeregisterTargetsOperation
    
    * address PR comments and add UT
    
    * add mocks
    
    * reduce targets cache time to 5 minute

--- scripts/gen_mocks.sh
+++ scripts/gen_mocks.sh
@@ -1,1 +1,2 @@
 mockgen -destination=./mocks/controller-runtime/client/mock_client.go sigs.k8s.io/controller-runtime/pkg/client Client
+mockgen -destination=./mocks/aws/services/mock_elbv2.go sigs.k8s.io/aws-alb-ingress-controller/pkg/aws/services ELBV2
