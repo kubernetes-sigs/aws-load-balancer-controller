a9cdd7cecb45d5e1bb70a8bf2a60f86608589982
Yang Yang <yyyng@amazon.com>
2020-09-01 14:13:07 -0700

    add algorithms for string & map

--- pkg/algorithm/maps.go
+++ pkg/algorithm/maps.go
@@ -10,3 +10,18 @@ func MapFindFirst(key string, maps ...map[string]string) (string, bool) {
 	}
 	return "", false
 }
+
+// MergeStringMap will merge multiple map[string]string into single one.
+// The merge is executed for maps argument in sequential order, if a key already exists, the value from previous map is kept.
+// e.g. MergeStringMap(map[string]string{"a": "1", "b": "2"}, map[string]string{"a": "3", "d": "4"}) == map[string]string{"a": "1", "b": "2", "d": "4"}
+func MergeStringMap(maps ...map[string]string) map[string]string {
+	ret := make(map[string]string)
+	for _, _map := range maps {
+		for k, v := range _map {
+			if _, ok := ret[k]; !ok {
+				ret[k] = v
+			}
+		}
+	}
+	return ret
+}
--- pkg/algorithm/maps_test.go
+++ pkg/algorithm/maps_test.go
@@ -78,3 +78,104 @@ func TestMapFindFirst(t *testing.T) {
 		})
 	}
 }
+
+func TestMergeStringMap(t *testing.T) {
+	type args struct {
+		maps []map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]string
+	}{
+		{
+			name: "merge two maps without duplicates",
+			args: args{
+				maps: []map[string]string{
+					{
+						"a": "1",
+						"b": "2",
+					},
+					{
+						"c": "3",
+						"d": "4",
+					},
+				},
+			},
+			want: map[string]string{
+				"a": "1",
+				"b": "2",
+				"c": "3",
+				"d": "4",
+			},
+		},
+		{
+			name: "merge two maps with duplicates",
+			args: args{
+				maps: []map[string]string{
+					{
+						"a": "1",
+						"b": "2",
+					},
+					{
+						"a": "3",
+						"d": "4",
+					},
+				},
+			},
+			want: map[string]string{
+				"a": "1",
+				"b": "2",
+				"d": "4",
+			},
+		},
+		{
+			name: "merge two maps when first map is nil",
+			args: args{
+				maps: []map[string]string{
+					nil,
+					{
+						"c": "3",
+						"d": "4",
+					},
+				},
+			},
+			want: map[string]string{
+				"c": "3",
+				"d": "4",
+			},
+		},
+		{
+			name: "merge two maps when second map is nil",
+			args: args{
+				maps: []map[string]string{
+					{
+						"a": "1",
+						"b": "2",
+					},
+					nil,
+				},
+			},
+			want: map[string]string{
+				"a": "1",
+				"b": "2",
+			},
+		},
+		{
+			name: "merge two maps when both map is nil",
+			args: args{
+				maps: []map[string]string{
+					nil,
+					nil,
+				},
+			},
+			want: map[string]string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := MergeStringMap(tt.args.maps...)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
