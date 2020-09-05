fad6a5b70154222c0ba2021cf85a5f320b8667d9
Kishor Joshi <joshikis@amazon.com>
2020-09-04 23:10:27 -0700

    add mocks for AWS EC2 service

--- scripts/gen_mocks.sh
+++ scripts/gen_mocks.sh
@@ -1,2 +1,3 @@
 mockgen -destination=./mocks/controller-runtime/client/mock_client.go sigs.k8s.io/controller-runtime/pkg/client Client
 mockgen -destination=./mocks/aws/services/mock_elbv2.go sigs.k8s.io/aws-alb-ingress-controller/pkg/aws/services ELBV2
+mockgen -destination=./mocks/aws/services/mock_ec2.go sigs.k8s.io/aws-alb-ingress-controller/pkg/aws/services EC2
