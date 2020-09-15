bac3fa90f1eb3de0e6c812dae45a0865189e4c8a
Yang Yang <yyyng@amazon.com>
2020-09-15 13:43:17 -0700

    add reconciler for securityGroup rules

--- pkg/networking/security_group_info_test.go
+++ pkg/networking/security_group_info_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_buildIPPermissionLabelForDescription(t *testing.T) {
+func Test_buildIPPermissionLabelsForDescription(t *testing.T) {
 	type args struct {
 		description string
 	}
@@ -56,7 +56,44 @@ func Test_buildIPPermissionLabelForDescription(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := buildIPPermissionLabelForDescription(tt.args.description)
+			got := buildIPPermissionLabelsForDescription(tt.args.description)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_buildIPPermissionDescriptionForLabels(t *testing.T) {
+	type args struct {
+		labels map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "one label",
+			args: args{
+				labels: map[string]string{
+					"key1": "value1",
+				},
+			},
+			want: "key1=value1",
+		},
+		{
+			name: "two label",
+			args: args{
+				labels: map[string]string{
+					"elbv2.k8s.aws/targetGroupBinding": "shared",
+					"key1":                             "value1",
+				},
+			},
+			want: "elbv2.k8s.aws/targetGroupBinding=shared,key1=value1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := buildIPPermissionDescriptionForLabels(tt.args.labels)
 			assert.Equal(t, tt.want, got)
 		})
 	}
