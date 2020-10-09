9e61fd9af2bd62766968cc41ee9eee4580b6bb7f
Kishor Joshi <joshikis@amazon.com>
2020-10-08 22:57:21 -0700

    add sync-period flag

--- pkg/runtime/config.go
+++ pkg/runtime/config.go
@@ -8,6 +8,7 @@ import (
 	"k8s.io/client-go/rest"
 	elbv2api "sigs.k8s.io/aws-load-balancer-controller/apis/elbv2/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"time"
 )
 
 const (
@@ -16,12 +17,14 @@ const (
 	flagLeaderElectionID        = "leader-election-id"
 	flagLeaderElectionNamespace = "leader-election-namespace"
 	flagWatchNamespace          = "watch-namespace"
+	flagSyncPeriod              = "sync-period"
 
 	defaultLeaderElectionID        = "aws-load-balancer-controller-leader"
 	defaultLeaderElectionNamespace = ""
 	defaultWatchNamespace          = corev1.NamespaceAll
 	defaultControllerPort          = 9443
 	defaultMetricsAddr             = ":8080"
+	defaultSyncPeriod              = 60 * time.Minute
 )
 
 var (
@@ -48,30 +51,39 @@ type Config interface {
 
 // NewConfig constructs a new Config object
 func NewConfig() Config {
-	return &defaultConfig{options: ctrl.Options{
-		Scheme: scheme,
-		Port:   defaultControllerPort,
-	}}
+	return &defaultConfig{
+		scheme:         scheme,
+		controllerPort: defaultControllerPort,
+	}
 }
 
 type defaultConfig struct {
-	options ctrl.Options
+	scheme                  *runtime.Scheme
+	controllerPort          int
+	metricsBindAddress      string
+	enableLeaderElection    bool
+	leaderElectionID        string
+	leaderElectionNamespace string
+	watchNamespace          string
+	syncPeriod              time.Duration
 }
 
 func (c *defaultConfig) BindFlags(fs *pflag.FlagSet) {
-	fs.StringVar(&c.options.MetricsBindAddress, flagMetricsAddr, defaultMetricsAddr,
+	fs.StringVar(&c.metricsBindAddress, flagMetricsAddr, defaultMetricsAddr,
 		"The address the metric endpoint binds to.")
-	fs.BoolVar(&c.options.LeaderElection, flagEnableLeaderElection, true,
+	fs.BoolVar(&c.enableLeaderElection, flagEnableLeaderElection, true,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	fs.StringVar(&c.options.LeaderElectionID, flagLeaderElectionID, defaultLeaderElectionID,
+	fs.StringVar(&c.leaderElectionID, flagLeaderElectionID, defaultLeaderElectionID,
 		"Name of the leader election ID to use for this controller")
-	fs.StringVar(&c.options.LeaderElectionNamespace, flagLeaderElectionNamespace, defaultLeaderElectionNamespace,
+	fs.StringVar(&c.leaderElectionNamespace, flagLeaderElectionNamespace, defaultLeaderElectionNamespace,
 		"Name of the leader election ID to use for this controller")
-	fs.StringVar(&c.options.Namespace, flagWatchNamespace, defaultWatchNamespace,
+	fs.StringVar(&c.watchNamespace, flagWatchNamespace, defaultWatchNamespace,
 		`Namespace the controller watches for updates to Kubernetes objects.
 		This includes Ingresses, Services and all configuration resources. All
 		namespaces are watched if this parameter is left empty.`)
+	fs.DurationVar(&c.syncPeriod, flagSyncPeriod, defaultSyncPeriod,
+		"Period at which the controller forces the repopulation of its local object stores.")
 }
 
 func (c *defaultConfig) GetRestConfig() *rest.Config {
@@ -79,5 +91,14 @@ func (c *defaultConfig) GetRestConfig() *rest.Config {
 }
 
 func (c *defaultConfig) GetRuntimeOptions() ctrl.Options {
-	return c.options
+	return ctrl.Options{
+		Scheme:                  c.scheme,
+		Port:                    c.controllerPort,
+		MetricsBindAddress:      c.metricsBindAddress,
+		LeaderElection:          c.enableLeaderElection,
+		LeaderElectionID:        c.leaderElectionID,
+		LeaderElectionNamespace: c.leaderElectionNamespace,
+		Namespace:               c.watchNamespace,
+		SyncPeriod:              &c.syncPeriod,
+	}
 }
