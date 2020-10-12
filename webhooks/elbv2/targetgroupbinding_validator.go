7e63c975dfd7a2448e292be6a4420f95c8abcd01
Yang Yang <yyyng@amazon.com>
2020-10-12 14:15:01 -0700

    updating webhooks to use v1beta1 crd

--- webhooks/elbv2/targetgroupbinding_validator.go
+++ webhooks/elbv2/targetgroupbinding_validator.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 )
 
-const apiPathValidateELBv2TargetGroupBinding = "/validate-elbv2-k8s-aws-v1alpha1-targetgroupbinding"
+const apiPathValidateELBv2TargetGroupBinding = "/validate-elbv2-k8s-aws-v1beta1-targetgroupbinding"
 
 // NewTargetGroupBindingMutator returns a mutator for TargetGroupBinding CRD.
 func NewTargetGroupBindingValidator(logger logr.Logger) *targetGroupBindingValidator {
@@ -87,7 +87,7 @@ func (v *targetGroupBindingValidator) checkImmutableFields(tgb *elbv2api.TargetG
 	return nil
 }
 
-// +kubebuilder:webhook:path=/validate-elbv2-k8s-aws-v1alpha1-targetgroupbinding,mutating=false,failurePolicy=fail,groups=elbv2.k8s.aws,resources=targetgroupbindings,verbs=create;update,versions=v1alpha1,name=vtargetgroupbinding.elbv2.k8s.aws,sideEffects=None,webhookVersions=v1beta1
+// +kubebuilder:webhook:path=/validate-elbv2-k8s-aws-v1beta1-targetgroupbinding,mutating=false,failurePolicy=fail,groups=elbv2.k8s.aws,resources=targetgroupbindings,verbs=create;update,versions=v1beta1,name=vtargetgroupbinding.elbv2.k8s.aws,sideEffects=None,webhookVersions=v1beta1
 
 func (v *targetGroupBindingValidator) SetupWithManager(mgr ctrl.Manager) {
 	mgr.GetWebhookServer().Register(apiPathValidateELBv2TargetGroupBinding, webhook.ValidatingWebhookForValidator(v))
