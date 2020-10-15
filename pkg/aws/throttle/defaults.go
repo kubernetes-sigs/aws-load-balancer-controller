4db6e2f5b05b7f2834b346df06ec3feae4cee73e
M00nF1sh <yyyng@amazon.com>
2020-10-14 17:48:00 -0700

    add throttle cfg for waf (#1505)

--- pkg/aws/throttle/defaults.go
+++ pkg/aws/throttle/defaults.go
@@ -1,8 +1,40 @@
 package throttle
 
+import (
+	"github.com/aws/aws-sdk-go/service/wafregional"
+	"github.com/aws/aws-sdk-go/service/wafv2"
+	"golang.org/x/time/rate"
+	"regexp"
+)
+
 // NewDefaultServiceOperationsThrottleConfig returns a ServiceOperationsThrottleConfig with default settings.
 func NewDefaultServiceOperationsThrottleConfig() *ServiceOperationsThrottleConfig {
 	return &ServiceOperationsThrottleConfig{
-		value: map[string][]throttleConfig{},
+		value: map[string][]throttleConfig{
+			wafregional.ServiceID: {
+				{
+					operationPtn: regexp.MustCompile("^AssociateWebACL|DisassociateWebACL"),
+					r:            rate.Limit(0.5),
+					burst:        1,
+				},
+				{
+					operationPtn: regexp.MustCompile("^GetWebACLForResource|ListResourcesForWebACL"),
+					r:            rate.Limit(1),
+					burst:        1,
+				},
+			},
+			wafv2.ServiceID: {
+				{
+					operationPtn: regexp.MustCompile("^AssociateWebACL|DisassociateWebACL"),
+					r:            rate.Limit(0.5),
+					burst:        1,
+				},
+				{
+					operationPtn: regexp.MustCompile("^GetWebACLForResource|ListResourcesForWebACL"),
+					r:            rate.Limit(1),
+					burst:        1,
+				},
+			},
+		},
 	}
 }
