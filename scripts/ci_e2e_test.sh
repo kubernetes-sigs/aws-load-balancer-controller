2819f3e40f5daa87af1778071191226c48a2313e
M00nF1sh <yyyng@amazon.com>
2020-12-03 16:07:33 -0800

    update

--- scripts/ci_e2e_test.sh
+++ scripts/ci_e2e_test.sh
@@ -10,4 +10,5 @@ echo $PULL_NUMBER
 
 DOCKER_CLI_EXPERIMENTAL=enabled docker version
 DOCKER_CLI_EXPERIMENTAL=enabled docker pull docker.io/docker/dockerfile:experimental
+DOCKER_CLI_EXPERIMENTAL=enabled docker pull docker.io/library/golang:1.15.0
 DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build . --target bin
