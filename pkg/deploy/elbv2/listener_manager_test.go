e62deee3d89a61fd6deb2127c3c70939b24f2c34
yutachaos <18604471+yutachaos@users.noreply.github.com>
2021-03-08 23:30:46 +0900

    Added listener manager test specified multiple acm testcase.
    Signed-off-by: yutachaos <18604471+yutachaos@users.noreply.github.com>

--- pkg/deploy/elbv2/listener_manager_test.go
+++ pkg/deploy/elbv2/listener_manager_test.go
@@ -66,6 +66,60 @@ func Test_isSDKListenerSettingsDrifted(t *testing.T) {
 			},
 		},
 		{
+			name: "listener hasn't drifted if multiple acm specified",
+			args: args{
+				lsSpec: elbv2model.ListenerSpec{
+					Port:       80,
+					Protocol:   elbv2model.ProtocolHTTPS,
+					SSLPolicy:  awssdk.String("ELBSecurityPolicy-FS-1-2-Res-2019-08"),
+					ALPNPolicy: []string{"HTTP2Preferred"},
+					Certificates: []elbv2model.Certificate{
+						{
+							CertificateARN: awssdk.String("cert-arn1"),
+						},
+						{
+							CertificateARN: awssdk.String("cert-arn2"),
+						},
+					},
+				},
+				sdkLS: &elbv2sdk.Listener{
+					Port:     awssdk.Int64(80),
+					Protocol: awssdk.String("HTTPS"),
+					Certificates: []*elbv2sdk.Certificate{
+						{
+							CertificateArn: awssdk.String("cert-arn1"),
+							IsDefault:      awssdk.Bool(true),
+						},
+					},
+					DefaultActions: []*elbv2sdk.Action{
+						{
+							Type: awssdk.String("fixed-response"),
+							FixedResponseConfig: &elbv2sdk.FixedResponseActionConfig{
+								StatusCode: awssdk.String("404"),
+							},
+						},
+					},
+					SslPolicy:  awssdk.String("ELBSecurityPolicy-FS-1-2-Res-2019-08"),
+					AlpnPolicy: awssdk.StringSlice([]string{"HTTP2Preferred"}),
+				},
+				desiredDefaultCerts: []*elbv2sdk.Certificate{
+					{
+						CertificateArn: awssdk.String("cert-arn1"),
+						IsDefault:      awssdk.Bool(true),
+					},
+				},
+				desiredDefaultActions: []*elbv2sdk.Action{
+					{
+						Type: awssdk.String("fixed-response"),
+						FixedResponseConfig: &elbv2sdk.FixedResponseActionConfig{
+							StatusCode: awssdk.String("404"),
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
 			name: "Ignore ALPN configuration if not specified in model",
 			args: args{
 				lsSpec: elbv2model.ListenerSpec{
