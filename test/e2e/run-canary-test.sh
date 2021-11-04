1fd09b7c226717cc8cb2526a1866e4afcdb629b1
Chinmay Gadgil <cgadgil@amazon.com>
2021-11-03 19:03:43 -0700

    renamed file Addressed PR comments

--- test/e2e/run-canary-test.sh
+++ test/e2e/run-canary-test.sh
@@ -15,38 +15,34 @@ if [[ -z "${OS_OVERRIDE}" ]]; then
   OS_OVERRIDE=linux
 fi
 
-CLUSTER_INFO=$(aws eks describe-cluster --name $CLUSTER_NAME --region $REGION)
+GET_CLUSTER_INFO_CMD="aws eks describe-cluster --name $CLUSTER_NAME --region $REGION"
+
+if [[ -z "${ENDPOINT}" ]]; then
+  CLUSTER_INFO=$($GET_CLUSTER_INFO_CMD)
+else
+  CLUSTER_INFO=$($GET_CLUSTER_INFO_CMD --endpoint $ENDPOINT)
+fi
 
 VPC_ID=$(echo $CLUSTER_INFO | jq -r '.cluster.resourcesVpcConfig.vpcId')
 SERVICE_ROLE_ARN=$(echo $CLUSTER_INFO | jq -r '.cluster.roleArn')
 ROLE_NAME=${SERVICE_ROLE_ARN##*/}
-
 ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 
 echo "VPC ID: $VPC_ID, Service Role ARN: $SERVICE_ROLE_ARN, Role Name: $ROLE_NAME"
-
-# Set up local resources
-echo "Attaching IAM Policy to Cluster Service Role"
-aws iam attach-role-policy \
-    --policy-arn arn:aws:iam::aws:policy/AmazonEKSVPCResourceController \
-    --role-name "$ROLE_NAME" > /dev/null
-
-echo "Enabling Pod ENI on aws-node"
-kubectl set env daemonset aws-node -n kube-system ENABLE_POD_ENI=true
 
 eksctl utils associate-iam-oidc-provider \
     --region $REGION \
     --cluster $CLUSTER_NAME \
     --approve
 
-echo "Create AWSLoadbalancerController IAM Policy"
+echo "Creating AWSLoadbalancerController IAM Policy"
 curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.2.1/docs/install/iam_policy.json
 
 aws iam create-policy \
     --policy-name AWSLoadBalancerControllerIAMPolicy \
     --policy-document file://iam-policy.json || true
 
-echo "Create IAM serviceaccount"
+echo "Creating IAM serviceaccount"
 eksctl create iamserviceaccount \
 --cluster=$CLUSTER_NAME \
 --namespace=kube-system \
@@ -71,16 +67,8 @@ echo "Starting the ginkgo test suite"
 
 (cd $SCRIPT_DIR && CGO_ENABLED=0 GOOS=$OS_OVERRIDE ginkgo -v -r -- --kubeconfig=$KUBE_CONFIG_PATH --cluster-name=$CLUSTER_NAME --aws-region=$REGION --aws-vpc-id=$VPC_ID || true)
 
-echo "Successfully finished the test suite"
-
-#Tear down local resources
-echo "Detaching the IAM Policy from Cluster Service Role"
-aws iam detach-role-policy \
-    --policy-arn arn:aws:iam::aws:policy/AmazonEKSVPCResourceController \
-    --role-name $ROLE_NAME || true
-
-echo "Disabling Pod ENI on aws-node"
-kubectl set env daemonset aws-node -n kube-system ENABLE_POD_ENI=false
+echo "Delete aws-load-balacner-controller"
+helm delete aws-load-balancer-controller -n kube-system
 
 echo "Delete iamserviceaccount"
 eksctl delete iamserviceaccount --name aws-load-balancer-controller --namespace kube-system --cluster $CLUSTER_NAME || true
@@ -88,5 +76,4 @@ eksctl delete iamserviceaccount --name aws-load-balancer-controller --namespace 
 echo "Delete TargetGroupBinding CRDs"
 kubectl delete -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
 
-echo "Delete aws-load-balacner-controller"
-helm delete aws-load-balancer-controller -n kube-system
+echo "Successfully finished the test suite"
