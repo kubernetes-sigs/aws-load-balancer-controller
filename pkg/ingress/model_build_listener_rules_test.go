a61da53533298da3b7c6f56e229cfbc3c3d422d6
oliviassss <sonyingy@amazon.com>
2022-04-05 14:30:49 -0700

    update sorting strategy

--- pkg/ingress/model_build_listener_rules_test.go
+++ pkg/ingress/model_build_listener_rules_test.go
@@ -18,17 +18,9 @@ func Test_defaultModelBuildTask_sortIngressPath(t *testing.T) {
 		want []networking.HTTPIngressPath
 	}{
 		{
-			name: "Paths sorted by length descending, no ties",
+			name: "Exact path only",
 			args: args{
 				paths: []networking.HTTPIngressPath{
-					{
-						Path:     "/abc",
-						PathType: (*networking.PathType)(awssdk.String("Prefix")),
-					},
-					{
-						Path:     "/aaa/bbb",
-						PathType: (*networking.PathType)(awssdk.String("Prefix")),
-					},
 					{
 						Path:     "",
 						PathType: (*networking.PathType)(awssdk.String("Exact")),
@@ -37,7 +29,32 @@ func Test_defaultModelBuildTask_sortIngressPath(t *testing.T) {
 			},
 			want: []networking.HTTPIngressPath{
 				{
-					Path:     "/aaa/bbb",
+					Path:     "",
+					PathType: (*networking.PathType)(awssdk.String("Exact")),
+				},
+			},
+		},
+		{
+			name: "Prefix path only",
+			args: args{
+				paths: []networking.HTTPIngressPath{
+					{
+						Path:     "/abc",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+					{
+						Path:     "/example",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+					{
+						Path:     "/tea",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+				},
+			},
+			want: []networking.HTTPIngressPath{
+				{
+					Path:     "/example",
 					PathType: (*networking.PathType)(awssdk.String("Prefix")),
 				},
 				{
@@ -45,13 +62,219 @@ func Test_defaultModelBuildTask_sortIngressPath(t *testing.T) {
 					PathType: (*networking.PathType)(awssdk.String("Prefix")),
 				},
 				{
-					Path:     "",
-					PathType: (*networking.PathType)(awssdk.String("Exact")),
+					Path:     "/tea",
+					PathType: (*networking.PathType)(awssdk.String("Prefix")),
 				},
 			},
 		},
 		{
-			name: "Paths length descending, when tie, the path with Exact path type precedes",
+			name: "ImplementationSpecific path only",
+			args: args{
+				paths: []networking.HTTPIngressPath{
+					{
+						Path:     "",
+						PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+					},
+					{
+						Path:     "/a",
+						PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+					},
+					{
+						Path:     "/test",
+						PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+					},
+				},
+			},
+			want: []networking.HTTPIngressPath{
+				{
+					Path:     "",
+					PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+				},
+				{
+					Path:     "/a",
+					PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+				},
+				{
+					Path:     "/test",
+					PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+				},
+			},
+		},
+		{
+			name: "Exact and prefix paths",
+			args: args{
+				paths: []networking.HTTPIngressPath{
+					{
+						Path:     "/abc",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+					{
+						Path:     "/aaa",
+						PathType: (*networking.PathType)(awssdk.String("Exact")),
+					},
+					{
+						Path:     "/aaa/bbb",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+					{
+						Path:     "/abc/abc/abc",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+				},
+			},
+			want: []networking.HTTPIngressPath{
+				{
+					Path:     "/aaa",
+					PathType: (*networking.PathType)(awssdk.String("Exact")),
+				},
+				{
+					Path:     "/abc/abc/abc",
+					PathType: (*networking.PathType)(awssdk.String("Prefix")),
+				},
+				{
+					Path:     "/aaa/bbb",
+					PathType: (*networking.PathType)(awssdk.String("Prefix")),
+				},
+				{
+					Path:     "/abc",
+					PathType: (*networking.PathType)(awssdk.String("Prefix")),
+				},
+			},
+		},
+		{
+			name: "Prefix and ImplementationSpecific paths",
+			args: args{
+				paths: []networking.HTTPIngressPath{
+					{
+						Path:     "/b",
+						PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+					},
+					{
+						Path:     "/ccc",
+						PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+					},
+					{
+						Path:     "/aaa",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+					{
+						Path:     "/example",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+				},
+			},
+			want: []networking.HTTPIngressPath{
+				{
+					Path:     "/example",
+					PathType: (*networking.PathType)(awssdk.String("Prefix")),
+				},
+				{
+					Path:     "/aaa",
+					PathType: (*networking.PathType)(awssdk.String("Prefix")),
+				},
+				{
+					Path:     "/b",
+					PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+				},
+				{
+					Path:     "/ccc",
+					PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+				},
+			},
+		},
+		{
+			name: "All three types",
+			args: args{
+				paths: []networking.HTTPIngressPath{
+					{
+						Path:     "/b",
+						PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+					},
+					{
+						Path:     "/ccc",
+						PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+					},
+					{
+						Path:     "/aaa",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+					{
+						Path:     "/example",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+					{
+						Path:     "/test",
+						PathType: (*networking.PathType)(awssdk.String("Exact")),
+					},
+				},
+			},
+			want: []networking.HTTPIngressPath{
+				{
+					Path:     "/test",
+					PathType: (*networking.PathType)(awssdk.String("Exact")),
+				},
+				{
+					Path:     "/example",
+					PathType: (*networking.PathType)(awssdk.String("Prefix")),
+				},
+				{
+					Path:     "/aaa",
+					PathType: (*networking.PathType)(awssdk.String("Prefix")),
+				},
+				{
+					Path:     "/b",
+					PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+				},
+				{
+					Path:     "/ccc",
+					PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			task := &defaultModelBuildTask{}
+			got, _ := task.sortIngressPaths(tt.args.paths)
+			assert.Equal(t, got, tt.want)
+		})
+	}
+}
+
+func Test_defaultModelBuildTask_classifyIngressPathsByType(t *testing.T) {
+	type args struct {
+		paths []networking.HTTPIngressPath
+	}
+	tests := []struct {
+		name                            string
+		args                            args
+		wantExactPaths                  []networking.HTTPIngressPath
+		wantPrefixPaths                 []networking.HTTPIngressPath
+		wantImplementationSpecificPaths []networking.HTTPIngressPath
+		wantErr                         error
+	}{
+		{
+			name: "Paths contain path with invalid pathType, return error",
+			args: args{
+				paths: []networking.HTTPIngressPath{
+					{
+						Path:     "/abc",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+					{
+						Path:     "/aaa/bbb",
+						PathType: (*networking.PathType)(awssdk.String("Prefix")),
+					},
+					{
+						Path:     "/ccc",
+						PathType: (*networking.PathType)(awssdk.String("xyz")),
+					},
+				},
+			},
+			wantErr: errors.New("unknown pathType for path /ccc"),
+		},
+		{
+			name: "Paths contain all three pathTypes",
 			args: args{
 				paths: []networking.HTTPIngressPath{
 					{
@@ -66,45 +289,49 @@ func Test_defaultModelBuildTask_sortIngressPath(t *testing.T) {
 						Path:     "/abc",
 						PathType: (*networking.PathType)(awssdk.String("Prefix")),
 					},
+					{
+						Path:     "/ccc",
+						PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+					},
 				},
 			},
-			want: []networking.HTTPIngressPath{
-				{
-					Path:     "/aaa/bbb",
-					PathType: (*networking.PathType)(awssdk.String("Prefix")),
-				},
+			wantExactPaths: []networking.HTTPIngressPath{
 				{
 					Path:     "/aaa",
 					PathType: (*networking.PathType)(awssdk.String("Exact")),
+				},
+			},
+			wantPrefixPaths: []networking.HTTPIngressPath{
+				{
+					Path:     "/aaa/bbb",
+					PathType: (*networking.PathType)(awssdk.String("Prefix")),
 				},
 				{
 					Path:     "/abc",
 					PathType: (*networking.PathType)(awssdk.String("Prefix")),
 				},
 			},
+			wantImplementationSpecificPaths: []networking.HTTPIngressPath{
+				{
+					Path:     "/ccc",
+					PathType: (*networking.PathType)(awssdk.String("ImplementationSpecific")),
+				},
+			},
 		},
 		{
-			name: "Paths are the same, the path with Exact path type precedes",
+			name: "only exact path",
 			args: args{
 				paths: []networking.HTTPIngressPath{
-					{
-						Path:     "/aaa",
-						PathType: (*networking.PathType)(awssdk.String("Prefix")),
-					},
 					{
 						Path:     "/aaa",
 						PathType: (*networking.PathType)(awssdk.String("Exact")),
 					},
 				},
 			},
-			want: []networking.HTTPIngressPath{
+			wantExactPaths: []networking.HTTPIngressPath{
 				{
 					Path:     "/aaa",
 					PathType: (*networking.PathType)(awssdk.String("Exact")),
-				},
-				{
-					Path:     "/aaa",
-					PathType: (*networking.PathType)(awssdk.String("Prefix")),
 				},
 			},
 		},
@@ -112,8 +339,15 @@ func Test_defaultModelBuildTask_sortIngressPath(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			task := &defaultModelBuildTask{}
-			got := task.sortIngressPaths(tt.args.paths)
-			assert.Equal(t, got, tt.want)
+			gotExactPaths, gotPrefixPaths, gotImplementationSpecificPaths, err := task.classifyIngressPathsByType(tt.args.paths)
+			if tt.wantErr != nil {
+				assert.EqualError(t, err, tt.wantErr.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, gotExactPaths, tt.wantExactPaths)
+				assert.Equal(t, gotPrefixPaths, tt.wantPrefixPaths)
+				assert.Equal(t, gotImplementationSpecificPaths, tt.wantImplementationSpecificPaths)
+			}
 		})
 	}
 }
