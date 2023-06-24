8e533b6766854b9bdda533e5086c74cb7466ee77
komisan19 <18901496+komisan19@users.noreply.github.com>
2023-06-24 14:58:44 +0900

    test/framework: replace deprecated ioutil

--- test/framework/http/response.go
+++ test/framework/http/response.go
@@ -1,8 +1,8 @@
 package http
 
 import (
-	"io/ioutil"
 	gohttp "net/http"
+	"os"
 )
 
 type Response struct {
@@ -12,7 +12,7 @@ type Response struct {
 
 func buildResponse(resp *gohttp.Response) (Response, error) {
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := os.ReadAll(resp.Body)
 	if err != nil {
 		return Response{}, err
 	}
