9581007e433e1d56b31952fa2797ed4dc819e9a5
Umesh Poswal <umesh.poswal@smartnews.com>
2023-06-30 17:53:19 +0900

    feat: disable sg rule management for ingresses - add test coverage

--- pkg/ingress/model_builder_test.go
+++ pkg/ingress/model_builder_test.go
@@ -2273,6 +2273,446 @@ func Test_defaultModelBuilder_Build(t *testing.T) {
 			wantErr: "backendSG feature is required to manage worker node SG rules when frontendSG manually specified",
 		},
 		{
+			name: "Ingress - with SG annotation, backend SG rule management disabled, managed backend sg set to true",
+			env: env{
+				svcs: []*corev1.Service{ns_1_svc_1, ns_1_svc_2, ns_1_svc_3},
+			},
+			fields: fields{
+				resolveViaDiscoveryCalls: []resolveViaDiscoveryCall{resolveViaDiscoveryCallForInternalLB},
+				listLoadBalancersCalls:   []listLoadBalancersCall{listLoadBalancerCallForEmptyLB},
+				describeSecurityGroupsResult: []describeSecurityGroupsResult{
+					{
+						securityGroups: []*ec2sdk.SecurityGroup{
+							{
+								GroupId: awssdk.String("sg-manual"),
+							},
+						},
+					},
+				},
+				backendSecurityGroup:          "sg-backend",
+				enableBackendSG:               true,
+				enableBackendSGRuleManagement: false,
+			},
+			args: args{
+				ingGroup: Group{
+					ID: GroupID{Namespace: "ns-1", Name: "ing-1"},
+					Members: []ClassifiedIngress{
+						{
+							Ing: &networking.Ingress{
+								ObjectMeta: metav1.ObjectMeta{
+									Namespace: "ns-1",
+									Name:      "ing-1",
+									Annotations: map[string]string{
+										"alb.ingress.kubernetes.io/security-groups":                     "sg-manual",
+										"alb.ingress.kubernetes.io/scheme":                              "internet-facing",
+										"alb.ingress.kubernetes.io/target-type":                         "instance",
+										"alb.ingress.kubernetes.io/manage-backend-security-group-rules": "true",
+									},
+								},
+								Spec: networking.IngressSpec{
+									Rules: []networking.IngressRule{
+										{
+											Host: "app-2.example.com",
+											IngressRuleValue: networking.IngressRuleValue{
+												HTTP: &networking.HTTPIngressRuleValue{
+													Paths: []networking.HTTPIngressPath{
+														{
+															Path: "/svc-3",
+															Backend: networking.IngressBackend{
+																Service: &networking.IngressServiceBackend{
+																	Name: ns_1_svc_3.Name,
+																	Port: networking.ServiceBackendPort{
+																		Name: "https",
+																	},
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantStackPatch: `
+{
+	"resources": {
+		"AWS::EC2::SecurityGroup": null,
+		"AWS::ElasticLoadBalancingV2::ListenerRule": {
+			"80:1": {
+				"spec": {
+					"actions": [
+						{
+							"forwardConfig": {
+								"targetGroups": [
+									{
+										"targetGroupARN": {
+											"$ref": "#/resources/AWS::ElasticLoadBalancingV2::TargetGroup/ns-1/ing-1-svc-3:https/status/targetGroupARN"
+										}
+									}
+								]
+							},
+							"type": "forward"
+						}
+					],
+					"conditions": [
+						{
+							"field": "host-header",
+							"hostHeaderConfig": {
+								"values": [
+									"app-2.example.com"
+								]
+							}
+						},
+						{
+							"field": "path-pattern",
+							"pathPatternConfig": {
+								"values": [
+									"/svc-3"
+								]
+							}
+						}
+					]
+				}
+			},
+			"80:2": null,
+			"80:3": null
+		},
+		"AWS::ElasticLoadBalancingV2::LoadBalancer": {
+			"LoadBalancer": {
+				"spec": {
+					"name": "k8s-ns1-ing1-159dd7a143",
+					"scheme": "internet-facing",
+					"securityGroups": [
+						"sg-manual"
+					]
+				}
+			}
+		},
+		"AWS::ElasticLoadBalancingV2::TargetGroup": {
+			"ns-1/ing-1-svc-1:http": null,
+			"ns-1/ing-1-svc-2:http": null
+		},
+		"K8S::ElasticLoadBalancingV2::TargetGroupBinding": {
+			"ns-1/ing-1-svc-1:http": null,
+			"ns-1/ing-1-svc-2:http": null,
+			"ns-1/ing-1-svc-3:https": {
+				"spec": {
+					"template": {
+						"spec": {
+							"networking": null
+						}
+					}
+				}
+			}
+		}
+	}
+}`,
+		},
+		{
+			name: "Ingress - with backend SG rule management disabled, managed backend sg set to true",
+			env: env{
+				svcs: []*corev1.Service{ns_1_svc_1, ns_1_svc_2, ns_1_svc_3},
+			},
+			fields: fields{
+				resolveViaDiscoveryCalls:      []resolveViaDiscoveryCall{resolveViaDiscoveryCallForInternalLB},
+				listLoadBalancersCalls:        []listLoadBalancersCall{listLoadBalancerCallForEmptyLB},
+				backendSecurityGroup:          "sg-backend",
+				enableBackendSG:               true,
+				enableBackendSGRuleManagement: false,
+			},
+			args: args{
+				ingGroup: Group{
+					ID: GroupID{Namespace: "ns-1", Name: "ing-1"},
+					Members: []ClassifiedIngress{
+						{
+							Ing: &networking.Ingress{
+								ObjectMeta: metav1.ObjectMeta{
+									Namespace: "ns-1",
+									Name:      "ing-1",
+									Annotations: map[string]string{
+										"alb.ingress.kubernetes.io/scheme":      "internet-facing",
+										"alb.ingress.kubernetes.io/target-type": "instance",
+									},
+								},
+								Spec: networking.IngressSpec{
+									Rules: []networking.IngressRule{
+										{
+											Host: "app-2.example.com",
+											IngressRuleValue: networking.IngressRuleValue{
+												HTTP: &networking.HTTPIngressRuleValue{
+													Paths: []networking.HTTPIngressPath{
+														{
+															Path: "/svc-3",
+															Backend: networking.IngressBackend{
+																Service: &networking.IngressServiceBackend{
+																	Name: ns_1_svc_3.Name,
+																	Port: networking.ServiceBackendPort{
+																		Name: "https",
+																	},
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantStackPatch: `
+{
+	"resources": {
+		"AWS::EC2::SecurityGroup": {
+			"ManagedLBSecurityGroup": {
+				"spec": {
+					"ingress": [
+						{
+							"fromPort": 80,
+							"ipProtocol": "tcp",
+							"ipRanges": [
+								{
+									"cidrIP": "0.0.0.0/0"
+								}
+							],
+							"toPort": 80
+						}
+					]
+				}
+			}
+		},
+		"AWS::ElasticLoadBalancingV2::ListenerRule": {
+			"80:1": {
+				"spec": {
+					"actions": [
+						{
+							"forwardConfig": {
+								"targetGroups": [
+									{
+										"targetGroupARN": {
+											"$ref": "#/resources/AWS::ElasticLoadBalancingV2::TargetGroup/ns-1/ing-1-svc-3:https/status/targetGroupARN"
+										}
+									}
+								]
+							},
+							"type": "forward"
+						}
+					],
+					"conditions": [
+						{
+							"field": "host-header",
+							"hostHeaderConfig": {
+								"values": [
+									"app-2.example.com"
+								]
+							}
+						},
+						{
+							"field": "path-pattern",
+							"pathPatternConfig": {
+								"values": [
+									"/svc-3"
+								]
+							}
+						}
+					]
+				}
+			},
+			"80:2": null,
+			"80:3": null
+		},
+		"AWS::ElasticLoadBalancingV2::LoadBalancer": {
+			"LoadBalancer": {
+				"spec": {
+					"name": "k8s-ns1-ing1-159dd7a143",
+					"scheme": "internet-facing",
+					"securityGroups": [
+						{
+							"$ref": "#/resources/AWS::EC2::SecurityGroup/ManagedLBSecurityGroup/status/groupID"
+						},
+						"sg-backend"
+					]
+				}
+			}
+		},
+		"AWS::ElasticLoadBalancingV2::TargetGroup": {
+			"ns-1/ing-1-svc-1:http": null,
+			"ns-1/ing-1-svc-2:http": null
+		},
+		"K8S::ElasticLoadBalancingV2::TargetGroupBinding": {
+			"ns-1/ing-1-svc-1:http": null,
+			"ns-1/ing-1-svc-2:http": null,
+			"ns-1/ing-1-svc-3:https": {
+				"spec": {
+					"template": {
+						"spec": {
+							"networking": null
+						}
+					}
+				}
+			}
+		}
+	}
+}`,
+		},
+		{
+			name: "Ingress - with backend SG rule management disabled, managed backend sg set to false",
+			env: env{
+				svcs: []*corev1.Service{ns_1_svc_1, ns_1_svc_2, ns_1_svc_3},
+			},
+			fields: fields{
+				resolveViaDiscoveryCalls:      []resolveViaDiscoveryCall{resolveViaDiscoveryCallForInternalLB},
+				listLoadBalancersCalls:        []listLoadBalancersCall{listLoadBalancerCallForEmptyLB},
+				backendSecurityGroup:          "sg-backend",
+				enableBackendSG:               false,
+				enableBackendSGRuleManagement: false,
+			},
+			args: args{
+				ingGroup: Group{
+					ID: GroupID{Namespace: "ns-1", Name: "ing-1"},
+					Members: []ClassifiedIngress{
+						{
+							Ing: &networking.Ingress{
+								ObjectMeta: metav1.ObjectMeta{
+									Namespace: "ns-1",
+									Name:      "ing-1",
+									Annotations: map[string]string{
+										"alb.ingress.kubernetes.io/scheme":      "internet-facing",
+										"alb.ingress.kubernetes.io/target-type": "instance",
+									},
+								},
+								Spec: networking.IngressSpec{
+									Rules: []networking.IngressRule{
+										{
+											Host: "app-2.example.com",
+											IngressRuleValue: networking.IngressRuleValue{
+												HTTP: &networking.HTTPIngressRuleValue{
+													Paths: []networking.HTTPIngressPath{
+														{
+															Path: "/svc-3",
+															Backend: networking.IngressBackend{
+																Service: &networking.IngressServiceBackend{
+																	Name: ns_1_svc_3.Name,
+																	Port: networking.ServiceBackendPort{
+																		Name: "https",
+																	},
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantStackPatch: `
+{
+	"resources": {
+		"AWS::EC2::SecurityGroup": {
+			"ManagedLBSecurityGroup": {
+				"spec": {
+					"ingress": [
+						{
+							"fromPort": 80,
+							"ipProtocol": "tcp",
+							"ipRanges": [
+								{
+									"cidrIP": "0.0.0.0/0"
+								}
+							],
+							"toPort": 80
+						}
+					]
+				}
+			}
+		},
+		"AWS::ElasticLoadBalancingV2::ListenerRule": {
+			"80:1": {
+				"spec": {
+					"actions": [
+						{
+							"forwardConfig": {
+								"targetGroups": [
+									{
+										"targetGroupARN": {
+											"$ref": "#/resources/AWS::ElasticLoadBalancingV2::TargetGroup/ns-1/ing-1-svc-3:https/status/targetGroupARN"
+										}
+									}
+								]
+							},
+							"type": "forward"
+						}
+					],
+					"conditions": [
+						{
+							"field": "host-header",
+							"hostHeaderConfig": {
+								"values": [
+									"app-2.example.com"
+								]
+							}
+						},
+						{
+							"field": "path-pattern",
+							"pathPatternConfig": {
+								"values": [
+									"/svc-3"
+								]
+							}
+						}
+					]
+				}
+			},
+			"80:2": null,
+			"80:3": null
+		},
+		"AWS::ElasticLoadBalancingV2::LoadBalancer": {
+			"LoadBalancer": {
+				"spec": {
+					"name": "k8s-ns1-ing1-159dd7a143",
+					"scheme": "internet-facing",
+					"securityGroups": [
+						{
+							"$ref": "#/resources/AWS::EC2::SecurityGroup/ManagedLBSecurityGroup/status/groupID"
+						}
+					]
+				}
+			}
+		},
+		"AWS::ElasticLoadBalancingV2::TargetGroup": {
+			"ns-1/ing-1-svc-1:http": null,
+			"ns-1/ing-1-svc-2:http": null
+		},
+		"K8S::ElasticLoadBalancingV2::TargetGroupBinding": {
+			"ns-1/ing-1-svc-1:http": null,
+			"ns-1/ing-1-svc-2:http": null,
+			"ns-1/ing-1-svc-3:https": {
+				"spec": {
+					"template": {
+						"spec": {
+							"networking": null
+						}
+					}
+				}
+			}
+		}
+	}
+}`,
+		},
+		{
 			name: "Ingress with IPv6 service",
 			env: env{
 				svcs: []*corev1.Service{ns_1_svc_ipv6},
