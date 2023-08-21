d041be2fb794ddc87f37903ada9394b8be84fb0b
Olivia Song <sonyingy@amazon.com>
2023-08-21 18:29:30 +0000

    add a debug line in ci script for timeout issue

--- scripts/ci_e2e_test.sh
+++ scripts/ci_e2e_test.sh
@@ -209,8 +209,8 @@ test_controller_image() {
   CERT_ID2="724963dd-f571-4f2c-b549-5c7d0e35e4b8"
   CERT_ID3="1001570b-1779-40c3-9b49-9a9a41e30058"
   CERTIFICATE_ARNS=${CERTIFICATE_ARNS:-"${CERTIFICATE_ARN_PREFIX}/${CERT_ID1},${CERTIFICATE_ARN_PREFIX}/${CERT_ID2},${CERTIFICATE_ARN_PREFIX}/${CERT_ID3}"}
-
-  ginkgo -v -r test/e2e -timeout 2h -- \
+  echo "!!!! running the ci_e2e_test.sh with changes"
+  ginkgo -v -r test/e2e --timeout 2h -- \
     --kubeconfig=${CLUSTER_KUBECONFIG} \
     --cluster-name=${CLUSTER_NAME} \
     --aws-region=${AWS_REGION} \
