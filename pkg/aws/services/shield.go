b40a257a7ae729e522482cc607c4846f07cc4a7b
Olivia Song <sonyingy@amazon.com>
2024-10-18 15:25:35 -0700

    Refactor aws cloud service and introduce a client provider (#3895)
    * add support for aws clients provider
    
    * refactor aws cloud service
    
    * fix typo

--- pkg/aws/services/shield.go
+++ pkg/aws/services/shield.go
@@ -2,9 +2,8 @@ package services
 
 import (
 	"context"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	shieldsdk "github.com/aws/aws-sdk-go-v2/service/shield"
-	"sigs.k8s.io/aws-load-balancer-controller/pkg/aws/endpoints"
+	"sigs.k8s.io/aws-load-balancer-controller/pkg/aws/provider"
 )
 
 type Shield interface {
@@ -15,33 +14,45 @@ type Shield interface {
 }
 
 // NewShield constructs new Shield implementation.
-func NewShield(cfg aws.Config, endpointsResolver *endpoints.Resolver) Shield {
-	customEndpoint := endpointsResolver.EndpointFor(shieldsdk.ServiceID)
-	// shield is only available as a global API in us-east-1.
-	client := shieldsdk.NewFromConfig(cfg, func(o *shieldsdk.Options) {
-		o.Region = "us-east-1"
-		o.BaseEndpoint = customEndpoint
-	})
-	return &shieldClient{shieldClient: client}
+func NewShield(awsClientsProvider provider.AWSClientsProvider) Shield {
+	return &shieldClient{
+		awsClientsProvider: awsClientsProvider,
+	}
 }
 
 // default implementation for Shield.
 type shieldClient struct {
-	shieldClient *shieldsdk.Client
+	awsClientsProvider provider.AWSClientsProvider
 }
 
 func (s *shieldClient) GetSubscriptionStateWithContext(ctx context.Context, input *shieldsdk.GetSubscriptionStateInput) (*shieldsdk.GetSubscriptionStateOutput, error) {
-	return s.shieldClient.GetSubscriptionState(ctx, input)
+	client, err := s.awsClientsProvider.GetShieldClient(ctx, "GetSubscriptionState")
+	if err != nil {
+		return nil, err
+	}
+	return client.GetSubscriptionState(ctx, input)
 }
 
 func (s *shieldClient) DescribeProtectionWithContext(ctx context.Context, input *shieldsdk.DescribeProtectionInput) (*shieldsdk.DescribeProtectionOutput, error) {
-	return s.shieldClient.DescribeProtection(ctx, input)
+	client, err := s.awsClientsProvider.GetShieldClient(ctx, "DescribeProtection")
+	if err != nil {
+		return nil, err
+	}
+	return client.DescribeProtection(ctx, input)
 }
 
 func (s *shieldClient) CreateProtectionWithContext(ctx context.Context, input *shieldsdk.CreateProtectionInput) (*shieldsdk.CreateProtectionOutput, error) {
-	return s.shieldClient.CreateProtection(ctx, input)
+	client, err := s.awsClientsProvider.GetShieldClient(ctx, "CreateProtection")
+	if err != nil {
+		return nil, err
+	}
+	return client.CreateProtection(ctx, input)
 }
 
 func (s *shieldClient) DeleteProtectionWithContext(ctx context.Context, input *shieldsdk.DeleteProtectionInput) (*shieldsdk.DeleteProtectionOutput, error) {
-	return s.shieldClient.DeleteProtection(ctx, input)
+	client, err := s.awsClientsProvider.GetShieldClient(ctx, "DeleteProtection")
+	if err != nil {
+		return nil, err
+	}
+	return client.DeleteProtection(ctx, input)
 }
