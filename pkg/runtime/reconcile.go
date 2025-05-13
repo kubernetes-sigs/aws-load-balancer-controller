249cd67cd67ee4b052ee9d2692bd3a7a1f023d29
Zachary Nixon <nixozach@amazon.com>
2025-05-13 10:15:42 -0700

    fix HandleReconcileError not detecting wrapped errors when using ErrorWithMetrics

--- pkg/runtime/reconcile.go
+++ pkg/runtime/reconcile.go
@@ -3,26 +3,41 @@ package runtime
 import (
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
+	errmetrics "sigs.k8s.io/aws-load-balancer-controller/pkg/error"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 // HandleReconcileError will handle errors from reconcile handlers, which respects runtime errors.
-func HandleReconcileError(err error, log logr.Logger) (ctrl.Result, error) {
-	if err == nil {
+func HandleReconcileError(inputErr error, log logr.Logger) (ctrl.Result, error) {
+	resolvedErr := handleNestedError(inputErr)
+	if resolvedErr == nil {
 		return ctrl.Result{}, nil
 	}
 
 	var requeueNeededAfter *RequeueNeededAfter
-	if errors.As(err, &requeueNeededAfter) {
+	if errors.As(resolvedErr, &requeueNeededAfter) {
 		log.V(1).Info("requeue after", "duration", requeueNeededAfter.Duration(), "reason", requeueNeededAfter.Reason())
 		return ctrl.Result{RequeueAfter: requeueNeededAfter.Duration()}, nil
 	}
 
 	var requeueNeeded *RequeueNeeded
-	if errors.As(err, &requeueNeeded) {
+	if errors.As(resolvedErr, &requeueNeeded) {
 		log.V(1).Info("requeue", "reason", requeueNeeded.Reason())
 		return ctrl.Result{Requeue: true}, nil
 	}
 
-	return ctrl.Result{}, err
+	return ctrl.Result{}, inputErr
+}
+
+func handleNestedError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	var wrappedError *errmetrics.ErrorWithMetrics
+	if errors.As(err, &wrappedError) {
+		return wrappedError.Err
+	}
+
+	return err
 }
