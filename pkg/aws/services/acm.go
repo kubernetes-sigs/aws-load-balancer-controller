bd8f8429cc4d1186ac5551133bb6f0a98ad1c949
Nathanael Liechti <technat@technat.ch>
2026-02-03 14:28:31 +0100

    feat: add more functionality to the ACM service

--- pkg/aws/services/acm.go
+++ pkg/aws/services/acm.go
@@ -2,6 +2,10 @@ package services
 
 import (
 	"context"
+	"time"
+
+	awssdk "github.com/aws/aws-sdk-go-v2/aws"
+
 	"github.com/aws/aws-sdk-go-v2/service/acm"
 	"github.com/aws/aws-sdk-go-v2/service/acm/types"
 	"sigs.k8s.io/aws-load-balancer-controller/pkg/aws/provider"
@@ -11,6 +15,10 @@ type ACM interface {
 	// wrapper to ListCertificatesPagesWithContext API, which aggregates paged results into list.
 	ListCertificatesAsList(ctx context.Context, input *acm.ListCertificatesInput) ([]types.CertificateSummary, error)
 	DescribeCertificateWithContext(ctx context.Context, req *acm.DescribeCertificateInput) (*acm.DescribeCertificateOutput, error)
+	ListTagsForCertificate(ctx context.Context, input *acm.ListTagsForCertificateInput) (*acm.ListTagsForCertificateOutput, error)
+	RequestCertificateWithContext(ctx context.Context, input *acm.RequestCertificateInput) (*acm.RequestCertificateOutput, error)
+	DeleteCertificateWithContext(ctx context.Context, input *acm.DeleteCertificateInput) (*acm.DeleteCertificateOutput, error)
+	WaitForCertificateIssuedWithContext(ctx context.Context, arn string, waitTime time.Duration) error
 }
 
 // NewACM constructs new ACM implementation.
@@ -41,10 +49,88 @@ func (c *acmClient) ListCertificatesAsList(ctx context.Context, input *acm.ListC
 	return result, nil
 }
 
+func (c *acmClient) ListTagsForCertificate(ctx context.Context, input *acm.ListTagsForCertificateInput) (*acm.ListTagsForCertificateOutput, error) {
+	client, err := c.awsClientsProvider.GetACMClient(ctx, "ListTagsForCertificate")
+	if err != nil {
+		return &acm.ListTagsForCertificateOutput{}, err
+	}
+
+	resp, err := client.ListTagsForCertificate(ctx, input)
+	if err != nil {
+		return &acm.ListTagsForCertificateOutput{}, err
+	}
+
+	return resp, nil
+}
+
 func (c *acmClient) DescribeCertificateWithContext(ctx context.Context, input *acm.DescribeCertificateInput) (*acm.DescribeCertificateOutput, error) {
 	client, err := c.awsClientsProvider.GetACMClient(ctx, "DescribeCertificate")
 	if err != nil {
 		return nil, err
 	}
 	return client.DescribeCertificate(ctx, input)
+}
+
+func (c *acmClient) RequestCertificateWithContext(ctx context.Context, req *acm.RequestCertificateInput) (*acm.RequestCertificateOutput, error) {
+	client, err := c.awsClientsProvider.GetACMClient(ctx, "RequestCertificate")
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.RequestCertificate(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (c *acmClient) WaitForCertificateIssuedWithContext(ctx context.Context, arn string, waitTime time.Duration) error {
+	client, err := c.awsClientsProvider.GetACMClient(ctx, "WaitForCertificate")
+	if err != nil {
+		return err
+	}
+
+	waiter := acm.NewCertificateValidatedWaiter(client, func(cvwo *acm.CertificateValidatedWaiterOptions) {
+		cvwo.Retryable = func(ctx context.Context, dci *acm.DescribeCertificateInput, dco *acm.DescribeCertificateOutput, err error) (bool, error) {
+			// the default retryable implementation from the awssdk has some limitations
+			// namely it doesn't wait for private certificates to become issued (they don't have any validation domains to wait for)
+			// and for amazon issued certificates it only checks if all domains are validated, not if the overall cert status is issued
+			// thus we use our own implementation to ensure we only return non-error if the cert is fully issued and thus ready to use
+			if err == nil {
+				if dco.Certificate.Status != types.CertificateStatusIssued {
+					return true, nil
+				}
+			} else {
+				return false, err
+			}
+
+			return false, nil
+		}
+	})
+
+	// return a description of the fullu issued certificate
+	req := &acm.DescribeCertificateInput{
+		CertificateArn: awssdk.String(arn),
+	}
+	err = waiter.Wait(ctx, req, waitTime)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *acmClient) DeleteCertificateWithContext(ctx context.Context, req *acm.DeleteCertificateInput) (*acm.DeleteCertificateOutput, error) {
+	client, err := c.awsClientsProvider.GetACMClient(ctx, "DeleteCertificate")
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.DeleteCertificate(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
